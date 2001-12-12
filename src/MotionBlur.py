132cbb58cc789d825e20958c248e128668d2bca9
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2001-12-12 12:29:37 +0000

    Moved timing functions from BlurredDrum to Stimulus, where they belong.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@23 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- src/MotionBlur.py
+++ src/MotionBlur.py
@@ -42,14 +42,14 @@ class ParamHolder: # dummy class to hold copy of params, basically plays role of
             pass
 
 class BlurTextureFamily:
-    def __init__(self,unblurred_filename,nominal_fps=180.0,maxSpeed=5000.0,numCachedTextures=10,cacheFunction='linear',blurKernel='boxcar'):
+    def __init__(self,unblurred_filename,target_fps=180.0,maxSpeed=2000.0,numCachedTextures=10,cacheFunction='linear',blurKernel='boxcar'):
         self.p = ParamHolder()
         # Compute blur family parameters
         self.p.orig_name = unblurred_filename
         self.orig = Image.open(self.p.orig_name)
         self.p.im_width = self.orig.size[0]
-        self.p.nominal_fps = nominal_fps
-        self.p.sec_per_frame = 1.0/self.p.nominal_fps
+        self.p.target_fps = target_fps
+        self.p.sec_per_frame = 1.0/self.p.target_fps
         self.p.maxSpeed = maxSpeed # dps
         self.p.numCachedTextures = numCachedTextures
         self.p.blurKernel = blurKernel
@@ -143,13 +143,13 @@ class BlurTextureFamily:
                 try:
                     blurred.save(filename)
                     p.filenames.append(filename)
-                    print "Saved '%s' (blurred for %f pixels per frame)"%(filename,pix_per_frame)
+                    print "Saved '%s' (blurred for %.1f degrees per second, %.2f pixels per frame)"%(filename,deg_per_sec,pix_per_frame)
                 except:
                     new_cache_valid = 0
                     print "Failed to save '%s'"%(filename)
             else: # use cache
                 filename = p.filenames[i]
-                print "Loading '%s' (blurred for %f pixels per frame)"%(filename,pix_per_frame)
+                print "Loading '%s' (blurred for %.1f degrees per second, %.2f pixels per frame)"%(filename,deg_per_sec,pix_per_frame)
                 blurred = Image.open(filename)
             tex = TextureFromPILImage( blurred )
             self.texGLIdList.append( tex.load() ) # create OpenGL texture object
@@ -168,85 +168,43 @@ class BlurTextureFamily:
                     
 class BlurredDrum(SpinningDrum):
 
-    def __init__(self,durationSec,unblurred_filename,posDegFunc,contrastFunc,numSides=30,radius=3.0,blur='gaussian',numCachedTextures=10,nominal_fps=180.0,projection=PerspectiveProjection()):
-        self.texs = BlurTextureFamily(unblurred_filename,numCachedTextures=numCachedTextures)
+    def __init__(self,durationSec,unblurred_filename,position_function,contrast_function,numSides=30,radius=3.0,blur='gaussian',numCachedTextures=10,target_fps=180.0,maxSpeed=2000.0,projection=PerspectiveProjection()):
+        self.texs = BlurTextureFamily(unblurred_filename,numCachedTextures=numCachedTextures,maxSpeed=maxSpeed,target_fps=target_fps)
         self.blurOn = 1
-        SpinningDrum.__init__(self,durationSec,TextureFromPILImage(self.texs.orig),posDegFunc,contrastFunc,numSides,radius,projection) # XXX should change so it doesn't load base texture again
+        self.last_time = 0.0
+        self.last_yrot = position_function(self.last_time)
+        SpinningDrum.__init__(self,durationSec,TextureFromPILImage(self.texs.orig),position_function,contrast_function,numSides,radius,projection) # XXX should change so it doesn't load base texture again
 
     def setBlurOn(self,on):
         self.blurOn = on
         
-    def glut_idle(self): # only called if running in GLUT
-        #global glut_window
-        glFinish()
-        curTimeAbs = getTime() # do this first
-        lastTime = self.curTime # even though it's confusing between these two instructions, this must be done now
-        self.curTime = curTimeAbs-self.startTimeAbs
-        lastyrot = self.yrot
-
-        self.contrast = self.cFunc(self.curTime)
-        self.yrot = self.posFunc(self.curTime)
-
-        delta_pos = self.yrot - lastyrot # degrees
-        delta_t = self.curTime - lastTime # seconds
-
-        self.texId = self.getTexId(delta_pos,delta_t)
-        
-        self.drawGLScene()
-        if self.curTime > self.durationSec:
-            self.stimulus_done()
-            #graphicsClose() # XXX Wrong! I just want to stop glutMainLoop!
-            #glutDestroyWindow(glut_window) # doesn't quit glutMainLoop
-
-    def glut_go(self): # only called if running in GLUT
-        self.startTimeAbs = getTime()
-        self.curTime = self.startTimeAbs
-        self.yrot = self.posFunc(0.0)
-        glutIdleFunc(self.glut_idle)
-        self.glut_idle() # set self.contrast and self.yrot
-        glutDisplayFunc(self.drawGLScene)
-        glutMainLoop()
-        
-    def go(self):
-        """Could put in C to run faster.
-
-        Bare bones timing routines.
-        """
-        startTimeAbs = getTime()
-        self.curTime = 0.0
-        self.yrot = self.posFunc(self.curTime)
-        lastyrot = self.yrot
-        lastTime = self.curTime
-        while (self.curTime <= self.durationSec):
-            self.contrast = self.cFunc(self.curTime)
-            self.yrot = self.posFunc(self.curTime)
-
-            delta_pos = self.yrot - lastyrot
-            delta_t = self.curTime - lastTime
-
-            lastTime = self.curTime
-
-            self.drawTimes3.append(getTime())
-            self.texId = self.getTexId(delta_pos,delta_t)
-            
-            lastyrot = self.yrot
-            self.drawGLScene()
-            curTimeAbs = getTime()
-            self.curTime = curTimeAbs-startTimeAbs
-        self.stimulus_done()
-
     def getTexId(self,delta_pos,delta_t):
-        if delta_t < 1.0e-6: # less than 1 microsecond (this should be less than a frame could possibly take to draw
-            vel_dps = 0
+        if self.blurOn: # Find the appropriate texture
+            if delta_t < 1.0e-6: # less than 1 microsecond (this should be less than a frame could possibly take to draw)
+                vel_dps = 0
+                speedIndex = 0
+            else:
+                vel_dps = delta_pos/delta_t
+                # Get the highest cached velocity less than or equal to the current velocity
+                speedIndex = nonzero(less_equal(self.texs.texGLSpeedsDps,abs(vel_dps)))[-1]
+        else: # In case motion blur is turned off
             speedIndex = 0
-        else:
-            vel_dps = delta_pos/delta_t
-            # Get the highest cached velocity less than or equal to the current velocity
-            speedIndex = nonzero(less_equal(self.texs.texGLSpeedsDps,abs(vel_dps)))[-1]
-        if not self.blurOn: # In case motion blur is turned off
-            speedIndex = 0
-        #print "%f: %f dps - %d"%(self.curTime,vel_dps,self.texs.texGLIdList[speedIndex])
-        #resident = glAreTexturesResident(self.texs.texGLIdList)
-        #print self.texs.texGLIdList
-        #print resident
         return self.texs.texGLIdList[speedIndex]
+
+    def drawGLScene(self):
+
+        # I know that self.cur_time has been set for me.
+        # Calculate my other variables from that.
+        
+        #self.contrast = self.cFunc(self.curTime) # Let SpinningDrum do this
+        self.yrot = self.position_function(self.cur_time)
+
+        delta_yrot = self.yrot - self.last_yrot # change in position (units: degrees)
+        delta_t = self.cur_time - self.last_time
+
+        self.texId = self.getTexId(delta_yrot,delta_t) # sets the texture object that SpinningDrum uses
+        SpinningDrum.drawGLScene(self) # call my base class to do most of the work
+        
+        # Set for next cycle
+        self.last_time = self.cur_time
+        self.last_yrot = self.yrot
