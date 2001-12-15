7f060291a3ee4325d3380ce0e3b5a6eb5cf5968f
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2001-12-15 10:15:22 +0000

    Bumped version number.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@30 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if build_sdl:
 
 # Normal distutils stuff
 setup(name="visionegg",
-      version = "0.5.4",
+      version = "0.6.0",
       description = "Vision Egg",
       url = 'http://visionegg.sourceforge.net',
       author = "Andrew Straw",
