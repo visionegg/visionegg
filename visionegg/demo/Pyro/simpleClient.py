ebeb366b87885c959f898172c395979b6adc59f1
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-02-27 08:05:48 +0000

    Got it all working!
    git-svn-id: https://visionegg.org/svn/trunk@96 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/Pyro/simpleClient.py
+++ visionegg/demo/Pyro/simpleClient.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
-import sys, socket
-import Pyro.naming, Pyro.core
+from VisionEgg.PyroHelpers import *
 
-Pyro.core.initClient()
-# locate the NS
-locator = Pyro.naming.NameServerLocator()
-print 'Searching Name Server...',
-ns = locator.getNS(Pyro.config.PYRO_NS_HOSTNAME)
+client = PyroClient()
 
-print 'Name Server found at',ns.URI.address,'('+(Pyro.protocol.getHostname(ns.URI.address) or '??')+') port',ns.URI.port
+go_object = client.get('go_object')
+angle_controller = client.get('angle_controller')
+on_controller = client.get('on_controller')
 
-# resolve the Pyro object
-print 'binding to object'
-try:
-    URI=ns.resolve('pyro_go_object')
-    print 'URI:',URI
-except Pyro.core.PyroError,x:
-    print 'Couldn\'t bind object, nameserver says:',x
-    raise SystemExit
-
-pyro_go_object = Pyro.core.getProxyForURI(URI)
-pyro_go_object.go()
+go_object.go()
--- visionegg/demo/Pyro/simpleServer.py
+++ visionegg/demo/Pyro/simpleServer.py
@@ -1,65 +1,34 @@
 #!/usr/bin/env python
 
-import sys
-
 from VisionEgg.Core import *
 from VisionEgg.AppHelper import *
+from VisionEgg.PyroHelpers import *
 
-import Pyro.naming
-import Pyro.core
-from Pyro.errors import PyroError,NamingError
+pyro_server = PyroServer()
 
-import GoObject
+# get visionegg stimulus ready to go
+screen = get_default_screen()
+projection = SimplePerspectiveProjection(fov_x=45.0)
+viewport = Viewport(screen,(0,0),screen.size,projection)
+stimulus = Stimulus()
+stimulus.init_gl()
+viewport.add_stimulus(stimulus)
+p = Presentation(duration_sec=5.0,viewports=[viewport])
 
-Pyro.config.PYRO_MULTITHREADED = 0 # Turn off multithreading -- kills OpenGL
+# make a controller, serve it via pyro, and glue it to the Presentation
+angle_controller = EvalStringPyroController('90.0*t')
+pyro_server.connect(angle_controller,'angle_controller')
+p.add_realtime_controller(stimulus.parameters,'yrot', angle_controller.eval)
 
-class PyroGoObject(Pyro.core.ObjBase, GoObject.GoObject):
-    pass
+on_controller = BiStatePyroController(1,0) # on during stimulus, off otherwise
+pyro_server.connect(on_controller,'on_controller')
+p.add_transitional_controller(stimulus.parameters,'on', on_controller.eval)
 
-def angle_as_function_of_time(t):
-    return 90.0*t # rotate at 90 degrees per second
+# initialize graphics to between presentations state
+p.between_presentations() 
 
-def main():
+# register the go function and serve it with pyro
+go_object = PyroGoClass(p.go)
+pyro_server.connect(go_object,'go_object')
 
-    # Start Pyro
-    Pyro.core.initServer()
-    PyroDaemon = Pyro.core.Daemon()
-    # locate the Pyro name server
-    locator = Pyro.naming.NameServerLocator()
-    print 'searching for Name Server...'
-    ns = locator.getNS(Pyro.config.PYRO_NS_HOSTNAME)
-    print 'Name Server found at',ns.URI.address,'('+(Pyro.protocol.getHostname(ns.URI.address) or '??')+') port',ns.URI.port
-    PyroDaemon.useNameServer(ns)
-
-    # get visionegg stimulus ready to go
-    screen = get_default_screen()
-    projection = SimplePerspectiveProjection(fov_x=45.0)
-    viewport = Viewport(screen,(0,0),screen.size,projection)
-    stimulus = Stimulus()
-    stimulus.init_gl()
-    viewport.add_stimulus(stimulus)
-    p = Presentation(duration_sec=5.0,viewports=[viewport])
-    p.add_realtime_controller(stimulus.parameters,'yrot', angle_as_function_of_time)
-    p.between_presentations() # initialize stuff..
-
-    # connect a new object implementation (first unregister previous one)
-    try:
-        ns.unregister('pyro_go_object')
-    except NamingError:
-        pass
-
-    g = PyroGoObject()
-    g.set_go_func(p.go)
-    
-    # connect new object implementation
-    PyroDaemon.connect(g,'pyro_go_object')
-    # enter the server loop
-    print 'Server object "pyro_go_object" ready.'
-    while 1:
-        PyroDaemon.handleRequests(3.0)
-
-        sys.stdout.write('.')
-        sys.stdout.flush()
-
-if __name__=="__main__":
-    main()
+pyro_server.mainloop()
