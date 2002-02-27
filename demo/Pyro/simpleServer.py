7ef8d083436650430b53b5971f95e94abed56bec
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-02-27 10:49:08 +0000

    Added blurb about needing Pyro Name Server running.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@110 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/Pyro/simpleServer.py
+++ demo/Pyro/simpleServer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+"""Script to run a VisionEgg Pyro server.
+
+You will need to have a Pyro Name Server running on your network for
+this to work.  It comes with the Pyro distribution as a script in the
+bin directory called ns.  Run it, run this script, and then run
+simpleClient.py from any computer on your network!"""
 
 from VisionEgg.Core import *
 from VisionEgg.AppHelper import *
