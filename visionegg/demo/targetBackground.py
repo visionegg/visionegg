8186fb2b57b147f958cf9f13092c005e2d6e03ad
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-02-28 00:41:08 +0000

    Modified demos so their output produces stuff good for web movies!
    git-svn-id: https://visionegg.org/svn/trunk@117 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/targetBackground.py
+++ visionegg/demo/targetBackground.py
@@ -6,8 +6,8 @@ from VisionEgg.AppHelper import *
 from VisionEgg.MoreStimuli import *
 from VisionEgg.MotionBlur import *
 
-target_velocity = 10.0
-drum_max_speed = 1000.0 # degrees per second
+target_velocity = 25.0
+drum_max_speed = 50.0 # degrees per second
 
 def x_as_function_of_time(t):
     return target_velocity*sin(0.1*2.0*math.pi*t)
@@ -19,7 +19,7 @@ def orientation(dummy):
     return 135.0
 
 def angle_as_function_of_time(t):
-    return drum_max_speed*math.cos(t) # rotate at 90 degrees per second
+    return drum_max_speed*math.cos(0.2*2*math.pi*t) # rotate at 90 degrees per second
 
 def one_during_experiment(t):
     if t < 0.0:
@@ -28,12 +28,17 @@ def one_during_experiment(t):
         return 1.0
 
 screen = get_default_screen()
-projection = SimplePerspectiveProjection(fov_x=45.0)
+projection = SimplePerspectiveProjection(fov_x=90.0)
 viewport = Viewport(screen,(0,0),screen.size,projection)
 target = Target2D()
 target.init_gl()
 viewport.add_overlay(target)
-drum = BlurredDrum(max_speed=drum_max_speed)
+try:
+    texture = TextureFromFile("orig.bmp") # try to open a texture file
+except:
+    texture = Texture(size=(256,16)) # otherwise, generate one
+
+drum = BlurredDrum(max_speed=drum_max_speed, texture=texture)
 drum.init_gl()
 viewport.add_stimulus(drum)
 
