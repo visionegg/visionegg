cf1b86107f1b0ab38ff56ca3e6ce480f0a232e81
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-03-14 14:14:36 +0000

    Made use new Teapot stimulus.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@151 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/simple.py
+++ demo/simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from VisionEgg.Core import *
+from VisionEgg.MoreStimuli import *
 from VisionEgg.AppHelper import *
 
 def angle_as_function_of_time(t):
@@ -9,7 +10,7 @@ def angle_as_function_of_time(t):
 screen = get_default_screen()
 projection = SimplePerspectiveProjection(fov_x=45.0)
 viewport = Viewport(screen,(0,0),screen.size,projection)
-stimulus = Stimulus()
+stimulus = Teapot()
 stimulus.init_gl()
 viewport.add_stimulus(stimulus)
 p = Presentation(duration_sec=5.0,viewports=[viewport])
