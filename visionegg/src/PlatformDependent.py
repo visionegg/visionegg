a03653b47ffb0780e9ae2b00dea270f8a5bb5183
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-04-28 03:35:39 +0000

    Added function sync_swap_with_vbl().
    git-svn-id: https://visionegg.org/svn/trunk@228 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/PlatformDependent.py
+++ visionegg/src/PlatformDependent.py
@@ -1,3 +1,6 @@
+import sys,os,string
+import VisionEgg.Core
+
 ############# Import Vision Egg C routines, if they exist #############
 try:
     from _maxpriority import *                  # pickup set_realtime() function
@@ -18,3 +21,26 @@ except:
         """Fake function definition.  Your system doesn't support the real function.
         """
         pass
+
+def sync_swap_with_vbl(failure_ok = 0):
+    """Synchronize buffer swapping and vertical retrace, if possible."""
+    success = 0
+    if sys.platform == "linux2":
+        # Unfotunately, cannot check do glGetString(GL_VENDOR) to
+        # check if drivers are nVidia because we have to do this
+        # before the drivers are started...
+        
+        # Assume drivers are nVidia
+        VisionEgg.Core.add_gl_assumption("GL_VENDOR","nvidia","Failed assumption made in PlatformDependent.py function sync_swap_with_vbl()")
+#        print "Assuming nVidia OpenGL drivers in PlatformDependent.py"
+        os.environ["__GL_SYNC_TO_VBLANK"] = "1"
+        success = 1
+        
+##    elif sys.platform == "win32":
+##        raise NotImplementedError
+
+    if not success:
+        if failure_ok:
+            print "Failed to synchronize buffer swapping and vertical retrace"
+        else:
+            raise VisionEgg.Core.EggError("Failed to synchronize buffer swapping and vertical retrace")
