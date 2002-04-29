9566bf7391d0fcfadbf3ecf3d5fb2d941b68ca47
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-04-29 04:34:07 +0000

    Used target.py as source to re-work makeMovie.py
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@235 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/makeMovie.py
+++ demo/makeMovie.py
@@ -1,47 +1,94 @@
 #!/usr/bin/env python
 
-import math, os
+# Make a movie of a black target moving across a white background.
+
+fps = 12.0 # frames per second
+
+########################################################
+#  Import various modules from the Vision Egg package  #
+########################################################
+
 from VisionEgg.Core import *
 from VisionEgg.AppHelper import *
 from VisionEgg.MoreStimuli import *
 
-fps = 12.0 # frames per second
+#################################
+#  Initialize the various bits  #
+#################################
+
+# Initialize OpenGL graphics screen
+screen = get_default_screen()
+
+# Set the background color (RGBA) white
+screen.parameters.bgcolor = (1.0,1.0,1.0,1.0)
+
+# Create a viewport on the screen
+viewport = Viewport(screen,(0,0),screen.size)
+
+# Create an orthographic projection that so that OpenGL object
+# coordinates are equal to window (pixel) coordinates.
+pixel_coords = OrthographicProjection(left=0,right=screen.size[0],
+                                      bottom=0,top=screen.size[1])
+
+# Create an instance of the Target2D class with appropriate parameters
+target = Target2D(projection = pixel_coords,
+                  width      = 25.0,
+                  height     = 10.0,
+                  color      = (0.0,0.0,0.0,1.0)) # Set the target color (RGBA) black
+
+# Initialize the target's OpenGL stuff
+target.init_gl()
+
+# Tell the viewport to draw the target
+viewport.add_stimulus(target)
+
+# Create an instance of the Presentation class
+p = Presentation(duration=(60,'frames'),viewports=[viewport])
+
+#################################
+#  Define controller functions  #
+#################################
+
+# A few variables used to calculate postion based on screen size
+mid_x = screen.size[0]/2.0
+mid_y = screen.size[1]/2.0
+if screen.size[0] < screen.size[1]:
+    max_vel = screen.size[0] * 0.4
+else:
+    max_vel = screen.size[1] * 0.4
 
 def x_as_function_of_frame(frame):
     t = float(frame) / fps
-    return 10.0*sin(0.2*2.0*math.pi*t)
+    return max_vel*sin(0.1*2.0*math.pi*t) + mid_x
 
 def y_as_function_of_frame(frame):
     t = float(frame) / fps
-    return 10.0*sin(0.2*2.0*math.pi*t)
+    return max_vel*sin(0.1*2.0*math.pi*t) + mid_y
 
-def orientation(dummy):
+def orientation(dummy_argument):
     return 135.0
 
-def one_during_experiment(t):
+def on_during_experiment(t):
     if t < 0.0:
-        return 0.0
+        return 0 # off between stimuli
     else:
-        return 1.0
+        return 1 # on during stimulus presentation
 
-screen = get_default_screen()
-projection = SimplePerspectiveProjection(fov_x=45.0)
-viewport = Viewport(screen,(0,0),screen.size,projection)
-target = Target2D()
-target.init_gl()
-viewport.add_stimulus(target)
-
-p = Presentation(duration=(60,'frames'),viewports=[viewport])
+#############################################################
+#  Connect the controllers with the variables they control  #
+#############################################################
 
 p.add_realtime_frame_controller(target.parameters,'x', x_as_function_of_frame)
 p.add_realtime_frame_controller(target.parameters,'y', y_as_function_of_frame)
 p.add_transitional_controller(target.parameters,'orientation', orientation)
-p.add_transitional_controller(target.parameters,'on', one_during_experiment)
+p.add_transitional_controller(target.parameters,'on', on_during_experiment)
+
+#######################
+#  Run the stimulus!  #
+#######################
 
 save_directory = os.path.join(VisionEgg.config.VISIONEGG_STORAGE,'movie')
 if not os.path.isdir(save_directory):
     print "Error: cannot save movie, because directory '%s' does not exist."%save_directory
 else:
     p.export_movie_go(frames_per_sec=fps,path=save_directory)
-
-
