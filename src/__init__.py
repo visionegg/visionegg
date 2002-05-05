3b030c7f42dec7afccad50aaa3170b6ec440fb70
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-05-05 14:50:41 +0000

    Moved setting of swap_buffers from Core.py.  Moved Parameters class from Core.py.  Added ClassWithParameters and took parameter setting code from Stimulus in Core.py.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@259 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- src/__init__.py
+++ src/__init__.py
@@ -32,6 +32,7 @@ except ImportError:
 
 import Configuration # a Vision Egg module
 import os, sys, time # standard python modules
+import pygame, pygame.display
 
 ############# Get config defaults #############
 config = Configuration.Config()
@@ -56,3 +57,70 @@ if sys.platform == 'win32':
     timing_func = time.clock
 else:
     timing_func = time.time    
+
+############# What function do we use to swap the buffers? #########
+swap_buffers = pygame.display.flip
+
+####################################################################
+#
+#        Parameters
+#
+####################################################################
+
+class Parameters:
+    """Hold stimulus parameters.
+
+    This abstraction of parameters is useful so that parameters can be
+    controlled via any number of means: evaluating a python function,
+    acquiring some data with a digital or analog input, etc.
+
+    All parameters (such as contrast, position, etc.) which should be
+    modifiable in runtime should be attributes of an instance of this
+    class, which serves as a nameholder for just this purpose.
+
+    Any class which has parameters should be subclass of ParameterUser.
+
+    See the Presentation class for more information about parameters
+    and controllers.
+    """
+    pass
+
+class ClassWithParameters:
+    """Base class for any Vision Egg class that uses parameters.
+
+    Any class that uses parameters potentially modifiable in realtime
+    should be a subclass of ClassWithParameters.  This class enforces
+    a standard system of parameter specification and default value
+    setting.  See classes Screen, Viewport, or any daughter class of
+    Stimulus for examples."""
+    
+    parameters_and_defaults = {} # empty for base class
+
+    def __init__(self,**kw):
+        """Create self.parameters and set values."""
+        
+        self.parameters = Parameters() # create self.parameters
+        
+        # Get a list of all classes this instance is derived from
+        classes = recursive_base_class_finder(self.__class__)
+
+        done_parameters_and_defaults = []
+
+        # Fill self.parameters with parameter names and set to default values
+        for klass in classes:
+            # If a class didn't override base class's parameters_and_defaults dictionary, don't deal with it twice
+            if klass.parameters_and_defaults not in done_parameters_and_defaults:
+                done_parameters_and_defaults.append(klass.parameters_and_defaults)
+                for parameter_name in klass.parameters_and_defaults.keys():
+                    # Make sure this parameter key/value pair doesn't exist already
+                    if hasattr(self.parameters,parameter_name):
+                        raise ValueError("More than one definition of parameter '%s'"%parameter_name)
+                    setattr(self.parameters,parameter_name,klass.parameters_and_defaults[parameter_name])
+
+        # Set self.parameters to the value in "kw"
+        for kw_parameter_name in kw.keys():
+            # Make sure this parameter exists already
+            if not hasattr(self.parameters,kw_parameter_name):
+                raise ValueError("parameter '%s' unknown"%kw_parameter_name)
+            else:
+                setattr(self.parameters,kw_parameter_name,kw[kw_parameter_name])
