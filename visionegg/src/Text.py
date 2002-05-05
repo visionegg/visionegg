70573838d53fc2e9d73ff1783f6d3abf3b51021d
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-05-05 14:47:35 +0000

    Uses lowerleft parameter instead of x and y. Removed projections.
    git-svn-id: https://visionegg.org/svn/trunk@257 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/Text.py
+++ visionegg/src/Text.py
@@ -24,29 +24,17 @@ class TextStimulus(VisionEgg.Core.Stimulus):
 
     It's a base class that defines the common interface between the
     other text stimuli."""
-    parameters_and_defaults = {'projection':None, # set in __init__
-                               'on':1,
+    parameters_and_defaults = {'on':1,
                                'color':(1.0,1.0,1.0,1.0),
-                               'x':0.5,
-                               'y':0.5,
+                               'lowerleft':(320.0,240.),
                                'text':'the string to display'}
-    def __init__(self,projection=None,**kw):
+    def __init__(self,**kw):
         apply(VisionEgg.Core.Stimulus.__init__,(self,),kw)
-        # Make sure the projection is set
-        if projection is not None:
-            # Use the user-supplied projection
-            self.parameters.projection = projection
-        else:
-            # No user-supplied projection, use the default. (Which is probably None.)
-            if self.parameters.projection is None:
-                # Since the default projection is None, set it to something useful.
-                # Assume spot is in center of viewport.
-                self.parameters.projection = VisionEgg.Core.OrthographicProjection(right=1.0,top=1.0)
 
 class BitmapText(TextStimulus):
     parameters_and_defaults = {'font':GLUT_BITMAP_TIMES_ROMAN_24}
-    def __init__(self,projection=None,**kw):
-        apply(TextStimulus.__init__,(self,projection),kw)
+    def __init__(self,**kw):
+        apply(TextStimulus.__init__,(self,),kw)
 
     def draw(self):
         if self.parameters.on:
@@ -56,10 +44,7 @@ class BitmapText(TextStimulus):
 
             glMatrixMode(GL_MODELVIEW)
             glLoadIdentity()
-            glTranslate(self.parameters.x,self.parameters.y,0.0)
-
-            # save and set the projection matrix
-            self.parameters.projection.push_and_set_gl_projection()
+            glTranslate(self.parameters.lowerleft[0],self.parameters.lowerleft[1],0.0)
 
             c = self.parameters.color
             glColor(c[0],c[1],c[2],c[3])
@@ -69,17 +54,14 @@ class BitmapText(TextStimulus):
             for char in self.parameters.text:
                 glutBitmapCharacter(self.parameters.font,ord(char))
 
-            # Now restore everything
-            glPopMatrix() # restore projection matrix
-
 class StrokeText(TextStimulus):
     parameters_and_defaults = {'font':GLUT_STROKE_ROMAN,
                                'orientation':0.0,
                                'linewidth':3.0, # in pixels
                                'anti_aliasing':1}
-    def __init__(self,projection=None,**kw):
+    def __init__(self,**kw):
         raise NotImplementedError("There's something broken with StrokeText, and I haven't figured it out yet!")
-        apply(TextStimulus.__init__,(self,projection),kw)
+        apply(TextStimulus.__init__,(self,),kw)
 
     def draw(self):
         if self.parameters.on:
@@ -88,11 +70,8 @@ class StrokeText(TextStimulus):
 
             glMatrixMode(GL_MODELVIEW)
             glLoadIdentity()
-            glTranslate(self.parameters.x,self.parameters.y,0.0)
+            glTranslate(self.parameters.lowerleft[0],self.parameters.lowerleft[1],0.0)
             glRotate(self.parameters.orientation,0.0,0.0,1.0)
-
-            # save and set the projection matrix
-            self.parameters.projection.push_and_set_gl_projection()
 
             c = self.parameters.color
             glColor(c[0],c[1],c[2],c[3])
@@ -117,5 +96,3 @@ class StrokeText(TextStimulus):
             # But this code does not draw the string!?!
             for char in self.parameters.text:
                 glutStrokeCharacter(self.parameters.font,ord(char))
-
-            glPopMatrix() # restore projection matrix
