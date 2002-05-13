25ea8e1792756ad99bd8375f5daec51943aeda73
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-05-13 12:17:01 +0000

    Added docstrings.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@270 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- src/_dout.c
+++ src/_dout.c
@@ -25,6 +25,9 @@
 static int lpt_works; // define a couple of global variables
 static int lpt_state;
 
+static char _dout_toggle_dout__doc__[] = 
+"Toggle DOUT.\n\nLinux x86 specific implementation: Toggles the lowest output\nbit of LPT0 (0x378)."
+
 static PyObject *_dout_toggle_dout(PyObject * self, PyObject * args)
 {
   //TRY(PyArg_ParseTuple(args,""));
@@ -35,6 +38,9 @@ static PyObject *_dout_toggle_dout(PyObject * self, PyObject * args)
   Py_INCREF(Py_None);
   return Py_None;
 }
+
+static char _dout_set_dout__doc__[] = 
+"Set DOUT.\n\nLinux x86 specific implementation: Sets the lowest output bit\nof LPT0 (0x378)."
 
 static PyObject *_dout_set_dout(PyObject * self, PyObject * args)
 {
@@ -51,8 +57,8 @@ static PyObject *_dout_set_dout(PyObject * self, PyObject * args)
 
 static PyMethodDef
 _dout_methods[] = {
-  { "toggle_dout", _dout_toggle_dout, 1},
-  { "set_dout", _dout_set_dout, 1},
+  { "toggle_dout", _dout_toggle_dout, 1, _dout_toggle_dout__doc__},
+  { "set_dout", _dout_set_dout, 1, _dout_set_dout__doc__},
   { NULL, NULL} /* sentinel */
 };
 
