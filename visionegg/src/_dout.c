27f14cec56e0bec4c6afbd062bf17b180005f703
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-05-15 07:42:26 +0000

    Compilefix: Fixed docstring
    git-svn-id: https://visionegg.org/svn/trunk@285 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/_dout.c
+++ visionegg/src/_dout.c
@@ -26,7 +26,7 @@ static int lpt_works; // define a couple of global variables
 static int lpt_state;
 
 static char _dout_toggle_dout__doc__[] = 
-"Toggle DOUT.\n\nLinux x86 specific implementation: Toggles the lowest output\nbit of LPT0 (0x378)."
+"Toggle DOUT.\n\nLinux x86 specific implementation: Toggles the lowest output\nbit of LPT0 (0x378).";
 
 static PyObject *_dout_toggle_dout(PyObject * self, PyObject * args)
 {
@@ -40,7 +40,7 @@ static PyObject *_dout_toggle_dout(PyObject * self, PyObject * args)
 }
 
 static char _dout_set_dout__doc__[] = 
-"Set DOUT.\n\nLinux x86 specific implementation: Sets the lowest output bit\nof LPT0 (0x378)."
+"Set DOUT.\n\nLinux x86 specific implementation: Sets the lowest output bit\nof LPT0 (0x378).";
 
 static PyObject *_dout_set_dout(PyObject * self, PyObject * args)
 {
