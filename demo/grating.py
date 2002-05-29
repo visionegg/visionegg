7fa0019396a3b3d750c4d23c74881bca0429b532
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-05-29 01:12:52 +0000

    Update to use new Controller class.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@321 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/grating.py
+++ demo/grating.py
@@ -6,23 +6,13 @@ from VisionEgg.Core import *
 from VisionEgg.Gratings import *
 from VisionEgg.AppHelper import *
 
-# Define a controller function to move the grating
-def phase_as_function_of_time(t):
-    temporal_frequency_hz = 5.0
-    return temporal_frequency_hz*t*360.0
-
-# Define a second controller to turn off grating between presentations
-def on_during_experiment(t):
-    if t < 0.0:
-        return 0 # off between stimuli
-    else:
-        return 1 # on during stimulus presentation
+temporal_frequency_hz = 5.0
+phase_controller = EvalStringController(
+    during_go_eval_string="%f*t*360.0"%(temporal_frequency_hz,)
+    )
     
 # Initialize OpenGL graphics screen.
 screen = get_default_screen()
-
-# Use viewport with pixel coordinate system for projection
-viewport = Viewport(screen=screen)
 
 # Calculate where the lower left corner of the grating must be to be centered
 grating_size = 256
@@ -35,15 +25,15 @@ stimulus = SinGrating2D(lowerleft   = ( grating_left , grating_bottom ),
                         wavelength  = grating_size / 10.0, # 10 cycles over this size
                         orientation = 90.0 )
 
-# Add the stimulus to the viewport
-viewport.add_stimulus(stimulus)
+# Create a viewport (with default pixel coordinate system)
+# with stimulus
+viewport = Viewport( screen=screen, stimuli=[stimulus] )
 
 # Create an instance of the Presentation class
 p = Presentation(duration=(5.0,'seconds'),viewports=[viewport])
 
 # Register the controller functions
-p.add_realtime_time_controller(stimulus,'phase', phase_as_function_of_time)
-p.add_transitional_controller(stimulus,'on', on_during_experiment)
+p.add_controller(stimulus,'phase', phase_controller)
 
 # Go!
 p.go()
