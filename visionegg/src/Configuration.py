779542f6a083a63a061af161e62b9d6ffceb7111
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-06-05 00:29:50 +0000

    Rewrite of retarded directory finding code.
    git-svn-id: https://visionegg.org/svn/trunk@352 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/Configuration.py
+++ visionegg/src/Configuration.py
@@ -7,18 +7,21 @@ the configuration options as attributes.
 This module searches for configuration options in the following order:
 environment variables, configuration file, generic defaults.
 
-The configuration file is by default VisionEgg.conf in the
-VISIONEGG_STORAGE directory, which is by default "VisionEgg/storage"
-in the base python directory.
+This module also determines the location of a couple directories.
+VISIONEGG_SYSTEM_DIR directory, which is by default "VisionEgg" in the
+base python directory (found in Python variable sys.prefix).  If it is
+not in VISIONEGG_SYSTEM_DIR, then VISIONEGG_USER_DIR is checked, which
+is by default "VisionEgg" in the directory specified by the
+environment variable HOME, if it exists, and os.curdir otherwise.
 
-You can create a file with defaults for your system.  This should be a
-text file with key/value pairs.  Blank lines and anything after the
-pound symbol ("#") will be treated as a comment.  Each key/value pairs
-should be on its own line and in the format "KEY=VALUE".  By default
-the file "VisionEgg.conf" from the VISIONEGG_STORAGE directory is used
-if present.  You can specify a different filename and directory by
-setting the environment variable VISIONEGG_CONFIG_FILE.
-"""
+You can create a configuration file that contains defaults for your
+system.  This should be a text file with key/value pairs.  Blank lines
+and anything after the pound symbol ("#") will be treated as a
+comment.  Each key/value pairs should be on its own line and in the
+format "KEY=VALUE".  By default the file "VisionEgg.cfg" from the
+VISIONEGG_SYSTEM_DIR or VISIONEGG_USER_DIR as specified above.
+However, You can specify a different filename and directory by setting
+the environment variable VISIONEGG_CONFIG_FILE.  """
 
 # This is the python source code for the config module of the Vision Egg package.
 #
@@ -33,21 +36,13 @@ setting the environment variable VISIONEGG_CONFIG_FILE.
 #
 ####################################################################
 
-# Special values:
-#  %u means os.environ['HOME'] if defined, os.curdir otherwise
-#  %b means sys.prefix if directory is writeable, os.environ['HOME'] if not
-#  %c means os.curdir
-
 defaults= {
-    'VISIONEGG_STORAGE':              '%b/VisionEgg/storage',
-    'VISIONEGG_DEFAULT_INIT':         'config', # could also be 'GUI'
+    'VISIONEGG_GUI_INIT':             0,
     'VISIONEGG_SCREEN_W':             640,
     'VISIONEGG_SCREEN_H':             480,
     'VISIONEGG_FULLSCREEN':           0,
     'VISIONEGG_PREFERRED_BPP':        32,
-    'VISIONEGG_SCREEN_BGCOLOR':       (0.5,0.5,0.5,0.0),
-    'VISIONEGG_TEXTURE_COMPRESSION':  0,
-    'VISIONEGG_MONITOR_REFRESH_HZ':   60,
+    'VISIONEGG_MONITOR_REFRESH_HZ':   60.0,
     'VISIONEGG_MAXPRIORITY':          0
     }
 
@@ -71,40 +66,28 @@ class Config:
     def __init__(self):
         reader = ConfigReader(defaults)
 
+        self.VISIONEGG_SYSTEM_DIR = os.path.join(sys.prefix,"VisionEgg")
+        self.VISIONEGG_USER_DIR = os.path.expanduser("~/VisionEgg")
+
         # See if there's an environment variable for the config file
         if 'VISIONEGG_CONFIG_FILE' in os.environ.keys():
             configFile = os.environ['VISIONEGG_CONFIG_FILE']
         else:
-            configFile = '' # If not, let the reader see if there's one in VISIONEGG_STORAGE
+            # Is there one in VISIONEGG_SYSTEM_DIR?
+            configFile = os.path.join(self.VISIONEGG_SYSTEM_DIR,"VisionEgg.cfg")
+            if not os.path.isfile(configFile):
+                configFile = os.path.join(self.VISIONEGG_USER_DIR,"VisionEgg.cfg")
+                if not os.path.isfile(configFile):
+                    configFile = None # No file, use defaults specified in environment variables then here
             
-        try:
-            reader.parse(configFile)
-        except EnvironmentError,x:
-            raise Exception("Error reading config file: "+configFile+": "+str(x))
-        self.__dict__.update(reader.items)
-        if(configFile):
-            self.__dict__['VISIONEGG_CONFIG_FILE'] = os.path.abspath(configFile)
-        else:
-            self.__dict__['VISIONEGG_CONFIG_FILE'] = ''
+        reader.parse(configFile)
 
-        # Create the storage directory if it doesn't exist yet
-        (drive,path)=os.path.splitdrive(self.VISIONEGG_STORAGE)
-        allowErrnos=[errno.EEXIST, errno.EBUSY]
-        if drive and (path=='\\' or path==''):
-            # On windows, mkdir of drive root throws EACCES...
-            allowErrnos.append(errno.EACCES)
-        try:
-            os.mkdir(self.VISIONEGG_STORAGE)
-        except OSError,x:
-            if x.errno == 13: #errno.EPERM:
-                self.VISIONEGG_STORAGE = os.path.join(os.environ['HOME'],"VisionEgg/storage")
-                try:
-                    os.mkdir(self.VISIONEGG_STORAGE)
-                except OSError,x:
-                    if x.errno not in allowErrnos:
-                        raise 
-            if x.errno not in allowErrnos:
-                raise 
+        self.__dict__.update(reader.items)
+
+        if(configFile):
+            self.VISIONEGG_CONFIG_FILE = os.path.abspath(configFile)
+        else:
+            self.VISIONEGG_CONFIG_FILE = ''
 
 class ConfigReader:
     def __init__(self, defaults):
@@ -115,7 +98,7 @@ class ConfigReader:
         done_config_file = 0
         if file:
             done_config_file = 1
-            
+
             file=open(file).readlines()
             for l in file:
                 match=self.matcher.match(l)
@@ -129,36 +112,11 @@ class ConfigReader:
         # Parse the environment variables (they override the config file)
         self.items.update(self.processEnv(defaults.keys()))
 
-        # First, fix up VISIONEGG_STORAGE because others depend on it.
-        self.items['VISIONEGG_STORAGE'] = self.treatSpecial(self.items['VISIONEGG_STORAGE'])
-
-        # Now see if there's a config file in VISIONEGG_STORAGE and use that if we haven't already done a config file
-        if not done_config_file:
-            file = os.path.join(self.items['VISIONEGG_STORAGE'],'VisionEgg.cfg')
-            if os.path.isfile(file):
-                file=open(file).readlines()
-                for l in file:
-                    match=self.matcher.match(l)
-                    if match:
-                        if defaults.has_key(match.group(1)):
-                            if match.group(2):
-                                self.items[match.group(1)] = match.group(2)
-                        else:
-                            raise KeyError('Unknown config in configfile: '+match.group(1))
-                
-                # Parse the environment variables again (they override the config file)
-                self.items.update(self.processEnv(defaults.keys()))
-        
         # Now fix up all other items:
         for i in self.items.keys():
-
-            newVal = self.treatSpecial(self.items[i])
-            if i in ('VISIONEGG_STORAGE',):
-                newVal=os.path.abspath(newVal)
-            # fix the variable type if it's an integer
+            # fix the type if it's an integer
             if type(defaults[i]) == type(42):
-                newVal = int(newVal)
-            self.items[i]= newVal
+                self.items[i] = int(self.items[i])
 
     def processEnv(self, keys):
         env={}
@@ -166,14 +124,3 @@ class ConfigReader:
             try: env[key] = os.environ[key]
             except KeyError: pass
         return env
-
-    def treatSpecial(self, value):
-        # treat special escape strings
-        if type(value)==type(""):
-            value = re.sub('%c',os.curdir,value)
-            value = re.sub('%b',sys.prefix,value)
-            try:
-                value = re.sub('%u',os.environ['HOME'],value)
-            except KeyError: # no environment variable 'HOME' -- use os.curdir
-                value = re.sub('%u',os.curdir,value)
-        return value
