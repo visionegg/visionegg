38b9bd0fcfda585d4919e6effbdaf65de0103ecc
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-06-11 04:25:08 +0000

    Quits even if in go loop
    git-svn-id: https://visionegg.org/svn/trunk@425 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/calibrate/quit_server.py
+++ visionegg/demo/calibrate/quit_server.py
@@ -8,6 +8,10 @@ from VisionEgg.Core import *
 
 client = PyroClient()
 
+duration_controller = client.get('duration_controller')
+duration_controller.set_during_go_value((0.0,'seconds'))
+duration_controller.evaluate_now()
+
 quit_controller = client.get('quit_controller')
 quit_controller.set_during_go_value(1)
 quit_controller.set_between_go_value(1)
