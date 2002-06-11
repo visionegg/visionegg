11583151d12c3532ff6fa6ecce6af7df7edde126
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-06-11 04:24:07 +0000

    Allow to run without text.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@424 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/calibrate/cal_server.py
+++ demo/calibrate/cal_server.py
@@ -6,7 +6,13 @@ from VisionEgg.MoreStimuli import *
 from VisionEgg.Gratings import *
 from VisionEgg.PyroHelpers import *
 from VisionEgg.DaqLPT import *
-from VisionEgg.Text import *
+try:
+    from VisionEgg.Text import *
+    # In case GLUT isn't working
+    text_ok = 1
+except:
+    text_ok = 0
+    pass
 import sys
 
 # Connet to Pyro Name Server
@@ -21,26 +27,35 @@ bg = SinGrating2D(center           = ( screen.size[0]/2.0, screen.size[1]/2.0 ),
                   spatial_freq     = 10.0 / screen.size[0], # units of cycles/pixel
                   temporal_freq_hz = 0.1)
 
-cal_spot = Target2D(size  = ( screen.size[0]/4.0, screen.size[1]/4.0 ),
+cal_spot = Target2D(size  = ( screen.size[0]/3.0, screen.size[1]/3.0 ),
                     center           = ( screen.size[0]/2.0, screen.size[1]/2.0 )
                     )
 
-go_text = BitmapText(text='In "go" loop',
-                     lowerleft=(10.0,10.0),
-                     color=(0.0,1.0,0.5,0.0))
+if text_ok:
+    go_symbol = BitmapText(text='In "go" loop',
+                           lowerleft=(10.0,10.0),
+                           color=(0.0,1.0,0.5,0.0))
 
-no_go_text = BitmapText(text='Between "go" loops',
-                        lowerleft=(10.0,10.0),
-                        color=(0.8,0.0,0.5,0.0))
-
-viewport = Viewport(screen=screen, stimuli=[bg,cal_spot,go_text,no_go_text])
+    no_go_symbol = BitmapText(text='Between "go" loops',
+                              lowerleft=(10.0,10.0),
+                              color=(0.8,0.0,0.5,0.0))
+else:
+    go_symbol = Target2D(center=(10.0,10.0),
+                         size=(10.0,10.0),
+                         color=(0.0,1.0,0.5,0.0))
+    
+    no_go_symbol = Target2D(center=(10.0,10.0),
+                            size=(10.0,10.0),
+                            color=(0.8,0.0,0.5,0.0))
+    
+viewport = Viewport(screen=screen, stimuli=[bg,cal_spot,go_symbol,no_go_symbol])
 
 p = Presentation(go_duration=(0.5,'seconds'),viewports=[viewport],check_events=1)
 
 # Show when in go loop vs. not
 
-p.add_controller(go_text,'on',ConstantController(1,0,eval_frequency=Controller.TRANSITIONS))
-p.add_controller(no_go_text,'on',ConstantController(0,1,eval_frequency=Controller.TRANSITIONS))
+p.add_controller(go_symbol,'on',ConstantController(1,0,eval_frequency=Controller.TRANSITIONS))
+p.add_controller(no_go_symbol,'on',ConstantController(0,1,eval_frequency=Controller.TRANSITIONS))
 
 # The main controller for the task
 
