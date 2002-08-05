a59f89006605032779135eb2af919ca493f1431a
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-08-05 00:31:23 +0000

    Updated to use multiple viewports and multiple models
    git-svn-id: https://visionegg.org/svn/trunk@599 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/lib3ds-demo.py
+++ visionegg/demo/lib3ds-demo.py
@@ -6,45 +6,88 @@ import os
 from VisionEgg import *
 from VisionEgg.Core import *
 from VisionEgg.Lib3DS import *
+from VisionEgg.MoreStimuli import *
+from Numeric import *
 
+building_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/bldskyl1.3ds")
+fly_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/a3dwasp.3ds")
+
+# Because we can't include the .3ds files with the Vision Egg
+# (licensing issues), check to see if they are there and give warning
+# if not.  If someone wants to donate some nice .3ds file to the
+# Vision Egg project, that would be great!
+
+if not os.access(building_file,os.R_OK):
+    raise RuntimeError(".3ds file \"%s\" not found.  Download from http://www.amazing3d.com/free/free.html and try again."%(building_file,))
+if not os.access(fly_file,os.R_OK):
+    raise RuntimeError(".3ds file \"%s\" not found.  Download from http://www.amazing3d.com/free/free.html and try again."%(building_file,))
+    
 screen = get_default_screen()
+screen.parameters.bgcolor = (0.0,0.0,0.3,0.0)
 
-filename = os.path.join("/3dmodels/insects","a3dwasp.3ds")
+aspect = float(screen.size[0]) / (screen.size[1] / 2.0)
 
-# Create the instance of Model3DS
-stimulus = Model3DS(filename=filename)
+buildings = Model3DS(filename=building_file)
+fly = Model3DS(filename=fly_file,
+               scale=array((10.0,10.0,10.0)),
+               orient_angle=90.0,
+               orient_axis=array((0.0,1.0,0.0)))
+ground = Rectangle3D(color=(0.0,0.6,0.0,0.0),
+                     vertex1=array(( -10000.0, 0.0, -10000.0)),
+                     vertex2=array(( -10000.0, 0.0,  10000.0)),
+                     vertex3=array((  10000.0, 0.0,  10000.0)),
+                     vertex4=array((  10000.0, 0.0, -10000.0)),
+                     )
 
-print "***PLEASE NOTE THAT LIB3DS SUPPORT IS INCOMPLETE***"
-##print
+# The top viewport shows tracks the fly from a circling POV above the
+# buildings
 
-##print "Dumping the nodes of %s"%(filename,)
-##stimulus.dump_nodes()
-##print
+projection1 = SimplePerspectiveProjection(fov_x=90.0,aspect_ratio=aspect)
+viewport1 = Viewport(screen=screen,
+                     lowerleft=(0,screen.size[1]/2),
+                     size=(screen.size[0],screen.size[1]/2),
+                     projection=projection1,
+                     stimuli=[ground,buildings,fly])
 
-##print "Dumping the materials of %s"%(filename,)
-##stimulus.dump_materials()
-##print
+# The bottom viewport is the fly eye view of the world
 
-##print "Dumping the meshes of %s"%(filename,)
-##stimulus.dump_meshes()
-##print
+projection2 = SimplePerspectiveProjection(fov_x=90.0,aspect_ratio=aspect)
+viewport2 = Viewport(screen=screen,
+                     lowerleft=(0,0),
+                     size=(screen.size[0],screen.size[1]/2),
+                     projection=projection2,
+                     stimuli=[ground,buildings])
 
-projection = SimplePerspectiveProjection(fov_x=90.0)
-viewport = Viewport(screen=screen,
-                    size=screen.size,
-                    projection=projection,
-                    stimuli=[stimulus])
+p = Presentation(go_duration=(10.0,'seconds'),viewports=[viewport1,viewport2])
 
-p = Presentation(go_duration=(5.0,'seconds'),viewports=[viewport])
+# This defines the fly's position in space as a function of time
+def fly_pos_f(t):
+    return Numeric.array((1000.0-t*300.0,400.0,-1000.0))
 
-def projection_matrix_f(t):
-    projection = SimplePerspectiveProjection(fov_x=55.0)
-    eye = (2.*sin(t),3.5,8.*cos(t))
-    camera_look_at = (0.0,0.5,0.0)
+# This defines the perspective projection of the top viewport
+def projection_matrix1_f(t):
+    # Create a projection that we use to manipulate a projection matrix.
+    projection = SimplePerspectiveProjection(fov_x=85.0,z_clip_near=1.,z_clip_far=100000.,aspect_ratio=aspect)
+    eye = fly_pos_f(t)
+    eye += (100.0*cos(0.5*t),10.0,30.0*sin(0.5*t))
+    camera_look_at = fly_pos_f(t)
     camera_up = (0.0,1.0,0.0)
     projection.look_at( eye, camera_look_at, camera_up)
     return projection.get_matrix()
 
-p.add_controller(projection,'matrix', FunctionController(during_go_func=projection_matrix_f))
+# This defines the perspective projection of the bottom viewport
+def projection_matrix2_f(t):
+    # Create a projection that we use to manipulate a projection matrix.
+    projection = SimplePerspectiveProjection(fov_x=135.0,z_clip_near=10.,z_clip_far=100000.,aspect_ratio=aspect)
+    eye = fly_pos_f(t)
+    camera_look_at = fly_pos_f(t) + array((-1.0,0.0,0.0)) # look ahead
+    camera_up = (0.0,1.0,0.0)
+    projection.look_at( eye, camera_look_at, camera_up)
+    return projection.get_matrix()
+
+
+p.add_controller(projection1,'matrix', FunctionController(during_go_func=projection_matrix1_f))
+p.add_controller(projection2,'matrix', FunctionController(during_go_func=projection_matrix2_f))
+p.add_controller(fly,'position', FunctionController(during_go_func=fly_pos_f))
 
 p.go()
