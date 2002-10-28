b10f2aecda23aebbc326610c0c356eaa7cef8f6b
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-10-28 06:23:18 +0000

    Added ability to show grid rather than grating
    git-svn-id: https://visionegg.org/svn/trunk@662 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/sphereGratingWindowed.py
+++ visionegg/demo/sphereGratingWindowed.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+show_grid = 0 # show grid on which grating is defined
+
 from VisionEgg import *
 from VisionEgg.Core import *
 from VisionEgg.SphereMap import *
@@ -7,7 +9,12 @@ from VisionEgg.Text import *
 import math, os
 import pygame
 
+if show_grid:
+    from VisionEgg.Textures import *
+
 mouse_position = (320.0, 240.0)
+elevation = 0.0
+azimuth = 0.0
 
 class MousePositionController( Controller ):
     def __init__(self):
@@ -27,15 +34,17 @@ class MousePositionController( Controller ):
     between_go_eval = during_go_eval
 
 def elevation_func(t=None):
-    global mouse_position, screen
+    global mouse_position, screen, elevation
     y = mouse_position[1]
     elevation = (float(y) / screen.size[1]) * 180 - 90
+    text.parameters.text = "Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation)
     return elevation
 
 def azimuth_func(t=None):
-    global mouse_position, screen
+    global mouse_position, screen, azimuth
     x = mouse_position[0]
     azimuth = (float(x) / screen.size[0]) * 180 - 90
+    text.parameters.text = "Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation)
     return azimuth
 
 def quit(event):
@@ -53,12 +62,18 @@ handle_event_callbacks = [(pygame.locals.QUIT, quit),
 screen = get_default_screen()
 
 projection = SimplePerspectiveProjection(fov_x=130.0)
-stimulus = SphereGrating(radius=1.0,
-                         spatial_freq_cpd=1.0/9.0,
-                         temporal_freq_hz=1.0)
+
+if show_grid:
+    filename = os.path.join(config.VISIONEGG_SYSTEM_DIR,"data/mercator.png")
+    texture = TextureFromFile(filename)
+    stimulus = SphereMap(texture=texture,shrink_texture_ok=1,stacks=100,slices=100)
+else:
+    stimulus = SphereGrating(radius=1.0,
+                             spatial_freq_cpd=1.0/9.0,
+                             temporal_freq_hz=1.0)
 mask = SphereWindow(radius=1.0*0.95,
                     window_shape_radius_parameter=20.0)
-text = BitmapText(text="Mouse moves window, press Esc to quit",lowerleft=(0,0))
+text = BitmapText(text="Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation),lowerleft=(0,0))
 viewport = Viewport(screen=screen,
                     size=screen.size,
                     projection=projection,
@@ -67,13 +82,23 @@ text_viewport = Viewport(screen=screen, # default (orthographic) viewport
                          stimuli=[text])
 p = Presentation(viewports=[viewport,text_viewport],
                  handle_event_callbacks=handle_event_callbacks)
+
 p.add_controller(None, None, MousePositionController() )
+
 elevation_controller = FunctionController(during_go_func=elevation_func,
                                           between_go_func=elevation_func)
+p.add_controller(mask,'window_center_elevation', elevation_controller)
+if show_grid:
+    p.add_controller(stimulus,'center_elevation', elevation_controller)
+else:
+    p.add_controller(stimulus,'grating_center_elevation', elevation_controller)
+    
 azimuth_controller = FunctionController(during_go_func=azimuth_func,
                                         between_go_func=azimuth_func)
-p.add_controller(mask,'window_center_elevation', elevation_controller)
-p.add_controller(stimulus,'grating_center_elevation', elevation_controller)
 p.add_controller(mask,'window_center_azimuth', azimuth_controller)
-p.add_controller(stimulus,'grating_center_azimuth', azimuth_controller)
+if show_grid:
+    p.add_controller(stimulus,'center_azimuth', azimuth_controller)
+else:
+    p.add_controller(stimulus,'grating_center_azimuth', azimuth_controller)
+
 p.run_forever()
