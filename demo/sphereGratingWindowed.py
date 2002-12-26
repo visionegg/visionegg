7ce0dfb05126386129b30da290fd4614e31b7b0c
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-12-26 22:14:55 +0000

    Major upgrade with onscreen descriptions, etc.
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@738 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/sphereGratingWindowed.py
+++ demo/sphereGratingWindowed.py
@@ -4,8 +4,10 @@ show_grid = 0 # show grid on which grating is defined
 
 from VisionEgg import *
 from VisionEgg.Core import *
+from VisionEgg.Gratings import *
 from VisionEgg.SphereMap import *
 from VisionEgg.Text import *
+from VisionEgg.Textures import *
 import math, os
 import pygame
 
@@ -18,7 +20,7 @@ azimuth = 0.0
 
 class MousePositionController( Controller ):
     def __init__(self):
-        #global mouse_position
+        global mouse_position
         Controller.__init__(self,
                             return_type=type(None),
                             eval_frequency=Controller.EVERY_FRAME)
@@ -33,72 +35,133 @@ class MousePositionController( Controller ):
 
     between_go_eval = during_go_eval
 
-def elevation_func(t=None):
+grating_orient_method = 'reorient stimulus' # start with this as default
+
+def az_el_controller(t=None):
     global mouse_position, screen, elevation
-    y = mouse_position[1]
-    elevation = (float(y) / screen.size[1]) * 180 - 90
-    text.parameters.text = "Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation)
-    return elevation
-
-def azimuth_func(t=None):
-    global mouse_position, screen, azimuth
-    x = mouse_position[0]
+    global grid_stimulus, grating_stimulus, mask
+    global grating_orient_method
+    x,y = mouse_position
     azimuth = (float(x) / screen.size[0]) * 180 - 90
-    text.parameters.text = "Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation)
-    return azimuth
-
+    elevation = (float(y) / screen.size[1]) * 180 - 90
+    text1.parameters.text = "Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation)
+    mask.parameters.window_center_azimuth = azimuth
+    mask.parameters.window_center_elevation = elevation
+    if grating_orient_method == 'normal':
+        grating_stimulus.parameters.grating_center_azimuth = 0.0
+        grating_stimulus.parameters.grating_center_elevation = 0.0
+        grid_stimulus.parameters.center_azimuth = 0.0
+        grid_stimulus.parameters.center_elevation = 0.0
+    else:
+        grid_stimulus.parameters.center_azimuth = azimuth
+        grid_stimulus.parameters.center_elevation = elevation
+        grating_stimulus.parameters.grating_center_azimuth = azimuth
+        grating_stimulus.parameters.grating_center_elevation = elevation
+    
 def quit(event):
     global p # get presentation instance
     p.parameters.quit = 1
 
+def mouse_button_down(event):
+    if event.button == 1:
+        global grating_orient_method
+        grating_orient_method = 'normal'
+
+def mouse_button_up(event):
+    if event.button == 1:
+        global grating_orient_method
+        grating_orient_method = 'reorient stimulus'
+
 def keydown(event):
-    global up, down, left, right
+    global grating_stimulus
     if event.key == pygame.locals.K_ESCAPE:
         quit(event)
+    elif event.key == pygame.locals.K_KP1:
+        grating_stimulus.parameters.orientation = 135.0
+    elif event.key == pygame.locals.K_KP2:
+        grating_stimulus.parameters.orientation = 90.0
+    elif event.key == pygame.locals.K_KP3:
+        grating_stimulus.parameters.orientation = 45.0
+    elif event.key == pygame.locals.K_KP6:
+        grating_stimulus.parameters.orientation = 0.0
+    elif event.key == pygame.locals.K_KP9:
+        grating_stimulus.parameters.orientation = 315.0
+    elif event.key == pygame.locals.K_KP8:
+        grating_stimulus.parameters.orientation = 270.0
+    elif event.key == pygame.locals.K_KP7:
+        grating_stimulus.parameters.orientation = 235.0
+    elif event.key == pygame.locals.K_KP4:
+        grating_stimulus.parameters.orientation = 180.0
+    elif event.key == pygame.locals.K_g:
+        grid_stimulus.parameters.on = 1
+        grating_stimulus.parameters.on = 0
+    elif event.key == pygame.locals.K_s:
+        grid_stimulus.parameters.on = 0
+        grating_stimulus.parameters.on = 1
         
 handle_event_callbacks = [(pygame.locals.QUIT, quit),
+                          (pygame.locals.MOUSEBUTTONDOWN, mouse_button_down),
+                          (pygame.locals.MOUSEBUTTONUP, mouse_button_up),
                           (pygame.locals.KEYDOWN, keydown)]
 
 screen = get_default_screen()
 
 projection = SimplePerspectiveProjection(fov_x=130.0)
+#projection.look_at( (0.0,0.0,0.0),
+#                    (-1.0,0.0,-1.0),
+#                    (0.0,1.0,0.0) )
 
-if show_grid:
-    filename = os.path.join(config.VISIONEGG_SYSTEM_DIR,"data/mercator.png")
-    texture = TextureFromFile(filename)
-    stimulus = SphereMap(texture=texture,shrink_texture_ok=1,stacks=100,slices=100)
-else:
-    stimulus = SphereGrating(radius=1.0,
-                             spatial_freq_cpd=1.0/9.0,
-                             temporal_freq_hz=1.0)
-mask = SphereWindow(radius=1.0*0.95,
-                    window_shape_radius_parameter=20.0)
-text = BitmapText(text="Mouse moves window, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation),lowerleft=(0,0))
+filename = os.path.join(config.VISIONEGG_SYSTEM_DIR,"data/mercator.png")
+texture = TextureFromFile(filename)
+grid_stimulus = SphereMap(texture = texture,
+                          shrink_texture_ok = 1,
+                          stacks = 100,
+                          slices = 100,
+                          on=0)
+try:
+    # We want the maximum number of samples possible, hopefully 2048
+    grating_stimulus = SphereGrating(num_samples=2048,
+                                     radius = 1.0,
+                                     spatial_freq_cpd = 1.0/9.0,
+                                     temporal_freq_hz = 1.0,
+                                     slices = 50,
+                                     stacks = 50)
+except NumSamplesTooLargeError:
+    grating_stimulus = SphereGrating(num_samples=1024,
+                                     radius = 1.0,
+                                     spatial_freq_cpd = 1.0/9.0,
+                                     temporal_freq_hz = 1.0,
+                                     slices = 50,
+                                     stacks = 50)
+    
+mask = SphereWindow(radius=1.0*0.90, # make sure window is inside sphere with grating
+                    window_shape_radius_parameter=40.0,
+                    slices=50,
+                    stacks=50)
+                    
+text0 = BitmapText(text="Demonstration of perspective distorted, windowed grating.",lowerleft=(0,105))
+text1 = BitmapText(text="Mouse moves mask, press Esc to quit. Az, El = (%05.1f, %05.1f)"%(azimuth,elevation),lowerleft=(0,80))
+text2 = BitmapText(text="'s' displays sinusoidal grating, 'g' displays (az, el) grid.",lowerleft=(0,55))
+text3 = BitmapText(text="Numeric keypad changes grating orientation.",lowerleft=(0,30))
+text4 = BitmapText(text="(Hold mouse button to prevent re-orienting stimulus with mask.)",lowerleft=(0,5))
 viewport = Viewport(screen=screen,
                     size=screen.size,
                     projection=projection,
-                    stimuli=[stimulus,mask]) # mask must be drawn after grating
+                    stimuli=[grid_stimulus,
+                             grating_stimulus,
+                             mask]) # mask must be drawn after others
 text_viewport = Viewport(screen=screen, # default (orthographic) viewport
-                         stimuli=[text])
+                         stimuli=[text0,
+                                  text1,
+                                  text2,
+                                  text3,
+                                  text4])
 p = Presentation(viewports=[viewport,text_viewport],
                  handle_event_callbacks=handle_event_callbacks)
 
 p.add_controller(None, None, MousePositionController() )
 
-elevation_controller = FunctionController(during_go_func=elevation_func,
-                                          between_go_func=elevation_func)
-p.add_controller(mask,'window_center_elevation', elevation_controller)
-if show_grid:
-    p.add_controller(stimulus,'center_elevation', elevation_controller)
-else:
-    p.add_controller(stimulus,'grating_center_elevation', elevation_controller)
-    
-azimuth_controller = FunctionController(during_go_func=azimuth_func,
-                                        between_go_func=azimuth_func)
-p.add_controller(mask,'window_center_azimuth', azimuth_controller)
-if show_grid:
-    p.add_controller(stimulus,'center_azimuth', azimuth_controller)
-else:
-    p.add_controller(stimulus,'grating_center_azimuth', azimuth_controller)
+p.add_controller(None,None,FunctionController(during_go_func=az_el_controller,
+                                              between_go_func=az_el_controller))
 
 p.run_forever()
