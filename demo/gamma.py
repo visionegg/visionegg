9f69b42a9a155f8acd3e4cd9d2348c809f95dfa3
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2002-12-26 22:02:53 +0000

    Major update with keyboard interaction and visibile progress
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@735 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/gamma.py
+++ demo/gamma.py
@@ -1,46 +1,66 @@
 #!/usr/bin/env python
 
-# Display a moving sinusoidal grating
-
 from VisionEgg.Core import *
 from VisionEgg.Gratings import *
+from VisionEgg.Text import *
 from Numeric import *
 import pygame
 
 if not hasattr(pygame.display,"set_gamma_ramp"):
     raise RuntimeError("Need pygame 1.5 or greater for set_gamma_ramp function.")
 
-def gamma_func(f):
-    r = arange(256).astype('i')
-    g = r
-    b = zeros((256,),'i')
-    if pygame.display.set_gamma_ramp(r,g,b):
-        print "Gamma set"
-    else:
-        print "Gamma failed."
-
 # Initialize OpenGL graphics screen.
 screen = get_default_screen()
 
-# Calculate where the lower left corner of the grating must be to be centered
-grating_size = 256
-grating_left   = (screen.size[0]/2) - (grating_size/2)
-grating_bottom = (screen.size[1]/2) - (grating_size/2)
-
 # Create the instance SinGrating with appropriate parameters
-stimulus = SinGrating2D(center      = ( grating_left , grating_bottom ),
-                        size        = ( grating_size , grating_size ),
-                        spatial_freq  = 10.0 / grating_size, # 10 cycles
-                        orientation = 90.0 )
+stimulus = SinGrating2D()
+text1 = BitmapText(text="Press - to decrease luminance range, + to increase.",lowerleft=(0,30))
+text2 = BitmapText(text="set_gamma_ramp(r,g,b):",lowerleft=(0,5))
 
 # Use viewport with pixel coordinate system for projection
-viewport = Viewport(screen=screen,stimuli=[stimulus])
+viewport = Viewport(screen=screen,stimuli=[stimulus,text1,text2])
+
+def quit(event):
+    global p # get presentation instance
+    p.parameters.quit = 1
+
+# set initial value
+gamma_scale = 1.0
+            
+def keydown(event):
+    if event.key == pygame.locals.K_ESCAPE:
+        quit(event)
+    elif event.key in [pygame.locals.K_KP_PLUS,
+                       pygame.locals.K_PLUS,
+                       pygame.locals.K_EQUALS]:
+        global gamma_scale
+        gamma_scale += 0.05
+        do_gamma()
+    elif event.key in [pygame.locals.K_KP_MINUS,
+                       pygame.locals.K_MINUS]:
+        global gamma_scale
+        gamma_scale -= 0.05
+        do_gamma()
+        
+def do_gamma():
+    global gamma_scale, text2
+    r = (arange(256)*256*gamma_scale).astype('i')
+    g = r
+    b = r
+    worked = pygame.display.set_gamma_ramp(r,g,b)
+    if worked:
+        text2.parameters.text = "set_gamma_ramp(r,g,b): success"
+    else:
+        text2.parameters.text = "set_gamma_ramp(r,g,b): failure"
+
+do_gamma() # set gamma once initially
+
+handle_event_callbacks = [(pygame.locals.QUIT, quit),
+                          (pygame.locals.KEYDOWN, keydown)]
 
 # Create an instance of the Presentation class
-p = Presentation(go_duration=(5.0,'seconds'),viewports=[viewport])
-
-# Register the controller functions
-p.add_controller(None,None,FunctionController(during_go_func=gamma_func,temporal_variables=Controller.FRAMES_SINCE_GO))
+p = Presentation(viewports=[viewport],
+                 handle_event_callbacks=handle_event_callbacks)
 
 # Go!
-p.go()
+p.run_forever()
