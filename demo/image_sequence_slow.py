99890016cf37aee64e0ff37dbbb3cde83bc1683c
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-01-30 00:54:49 +0000

    Removed image_sequence demo to replace with 2 new demos: a slow and a fast version
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@782 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ demo/image_sequence_slow.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""Display a sequence of images.
+
+The displayed image is changes by changing the TextureStimulus's
+Texture parameter, which will delete the old OpenGL texture object and
+create a new one.
+
+This method to switch images is slower than that demonstrated in the
+image_sequence_fast.py demo.
+
+The images need not all be the same size.
+"""
+
+from VisionEgg import *
+from VisionEgg.Core import *
+from VisionEgg.Textures import *
+
+import Image, ImageDraw
+
+import OpenGL.GL as gl
+
+num_images = 20
+duration_per_image = 0.1 # seconds
+
+image_size = (256,256)
+
+# Generate some images
+image_list = []
+for i in range(num_images):
+    image = Image.new("RGB",image_size,(0,0,255)) # Blue background
+    draw = ImageDraw.Draw(image)
+    line_x = image_size[0]/float(num_images) * i
+    draw.line((line_x, 0, line_x, image_size[1]), fill=(255,255,255))
+    image_list.append(image)
+
+texture_list = map(Texture,image_list) # create instances of Texture from images
+
+screen = get_default_screen()
+
+x = screen.size[0]/2.0 - image_size[0]/2.0
+y = screen.size[1]/2.0 - image_size[1]/2.0
+stimulus = TextureStimulus(texture=texture_list[0],
+                           lowerleft=(x,y),
+                           size=image_size)
+
+viewport = Viewport(screen=screen,
+                    stimuli=[stimulus])
+
+p = Presentation(go_duration=(num_images*duration_per_image,'seconds'),viewports=[viewport])
+
+def put_image(t):
+    i = int(t/duration_per_image) # choose image
+    stimulus.parameters.texture = texture_list[i]
+p.add_controller(None,None,FunctionController(during_go_func=put_image))
+
+p.go()
