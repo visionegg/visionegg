d5c698da8822bbefcba1077633226158ead343b9
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-02 05:09:38 +0000

    Initial commit
    git-svn-id: https://visionegg.org/svn/trunk@797 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ visionegg/demo/color_grating.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Sinusoidal color grating calculated in realtime."""
+
+############################
+#  Import various modules  #
+############################
+
+from VisionEgg.Core import *
+from VisionEgg.Gratings import *
+
+#####################################
+#  Initialize OpenGL window/screen  #
+#####################################
+
+screen = get_default_screen()
+
+######################################
+#  Create sinusoidal grating object  #
+######################################
+
+stimulus = SinGrating2DColor(color1 = (0.0,1.0,0.0,0.0), # RGBA, Alpha ignored
+                             color2 = (1.0,0.0,0.0,0.0), # RGBA, Alpha ignored
+                             center           = ( screen.size[0]/2.0, screen.size[1]/2.0 ),
+                             size             = ( 300.0 , 300.0 ),
+                             spatial_freq     = 10.0 / screen.size[0], # units of cycles/pixel
+                             temporal_freq_hz = 1.0,
+                             orientation      = 45.0 )
+
+###############################################################
+#  Create viewport - intermediary between stimuli and screen  #
+###############################################################
+
+viewport = Viewport( screen=screen, stimuli=[stimulus] )
+
+########################################
+#  Create presentation object and go!  #
+########################################
+
+p = Presentation(go_duration=(5.0,'seconds'),viewports=[viewport])
+p.go()
