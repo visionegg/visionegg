a7032f51bbac51c3808f05045591bef11ed21127
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-06 00:32:25 +0000

    Major: removed Pyro Name Server dependency
    git-svn-id: https://visionegg.org/svn/trunk@807 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/PyroClient.py
+++ visionegg/src/PyroClient.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2002 Andrew Straw.  Distributed under the terms of the
 # GNU Lesser General Public License (LGPL).
 
-import string
+import string, socket
 import VisionEgg
 
 __version__ = VisionEgg.release_name
@@ -12,20 +12,16 @@ __date__ = string.join(string.split('$Date$')[1:3], ' ')
 __author__ = 'Andrew Straw <astraw@users.sourceforge.net>'
 
 import Pyro.core
-import Pyro.naming
 
 class PyroClient:
     """Simplifies getting PyroControllers from a remote computer."""
-    def __init__(self):
-        """Initialize client and find Pyro Name Server."""
+    def __init__(self,server_hostname='',server_port=7766):
+        """Initialize Pyro client."""
         Pyro.core.initClient()
-        # locate the NS
-        locator = Pyro.naming.NameServerLocator()
-        #print 'Searching Name Server...',
-        self.ns = locator.getNS(Pyro.config.PYRO_NS_HOSTNAME)
-        #print 'Name Server found at',self.ns.URI.address,'('+(Pyro.protocol.getHostname(self.ns.URI.address) or '??')+') port',self.ns.URI.port
+        self.server_hostname = socket.getfqdn(server_hostname)
+        self.server_port = server_port
 
     def get(self,name):
-        """Return a remote Pyro object being served by a Pyro server."""
-        URI=self.ns.resolve(name)
+        """Return a remote Pyro object being served by Pyro server."""
+        URI = "PYROLOC://%s:%d/%s" % (self.server_hostname, self.server_port, name)
         return Pyro.core.getProxyForURI(URI)
