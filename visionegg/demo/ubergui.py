653557bdaaf9811ada93da55f0257c6398d9672e
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-06 00:28:04 +0000

    Minor: removed redundant Pyro error dialogs
    git-svn-id: https://visionegg.org/svn/trunk@802 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/ubergui.py
+++ visionegg/demo/ubergui.py
@@ -3,32 +3,18 @@
 import sys
 import Pyro
 import Tkinter, tkMessageBox
-from VisionEgg.PyroApps.UberClientGUI import client_list, AppWindow
+from VisionEgg.PyroApps.UberClientGUI import client_list, AppWindow, get_server
 
 # You can add your own controllers and GUIs to client_list
 
-try:
-    app_window = AppWindow(client_list=client_list)
-except Pyro.errors.PyroError, x:
-    uber_server_error = 0
-    if isinstance(x, Pyro.errors.ProtocolError) and str(x) == 'connection failed': # Can't find UberServer running on network
-        uber_server_error = 1
-    if isinstance(x, Pyro.errors.NamingError) and str(x) == 'name not found': # Can't find UberServer running on network
-        uber_server_error = 1
-    if uber_server_error:
-        tkMessageBox.showerror("Can't find UberServer","Can't find UberServer running on Pyro network.")
-        sys.exit(1)
-    elif str(x) in ["Name Server not responding","connection failed"]:
-        try:
-            tkMessageBox.showerror("Can't find Pyro Name Server","Can't find Pyro Name Server on network.")
-            sys.exit(1)
-        except:
-            raise # Can't find Pyro Name Server on network
-    else:
-        raise        
-        
-app_window.winfo_toplevel().wm_iconbitmap()
-app_window.pack(expand=1,fill=Tkinter.BOTH)
-app_window.winfo_toplevel().title("Vision Egg")
-app_window.winfo_toplevel().minsize(1,1)
-app_window.mainloop()
+result = get_server()
+if result:
+    hostname,port = result
+    app_window = AppWindow(client_list=client_list,
+                           server_hostname=hostname,
+                           server_port=port)
+
+    app_window.winfo_toplevel().wm_iconbitmap()
+    app_window.pack(expand=1,fill=Tkinter.BOTH)
+    app_window.winfo_toplevel().title("Vision Egg")
+    app_window.mainloop()
--- visionegg/demo/uberserver.py
+++ visionegg/demo/uberserver.py
@@ -8,14 +8,4 @@ from VisionEgg.PyroApps.UberServer import server_modules, start_server
 
 # You can add your own server modules to the server_modules list
 
-try:
-    start_server( server_modules )
-except Pyro.errors.PyroError, x:
-    if str(x) in ["Name Server not responding","connection failed"]:
-        try:
-            tkMessageBox.showerror("Can't find Pyro Name Server","Can't find Pyro Name Server on network.")
-            sys.exit(1)
-        except:
-            raise # Can't find Pyro Name Server on network
-    else:
-            raise        
+start_server( server_modules )
