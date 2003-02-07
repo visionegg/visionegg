8111119d231f89ac4a4bcaf39f7077773a91b463
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-07 12:15:46 +0000

    Minor: allow build on Python 2.1
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@821 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- swig_src/make.py
+++ swig_src/make.py
@@ -13,7 +13,7 @@ swig_command = "swig"
 if len(sys.argv) > 1:
     swig_command = sys.argv[1]
 
-if 'commands' in globals():
+if 'commands' in globals().keys():
     status, output = commands.getstatusoutput(swig_command + " -version")
     if status != 0:
         print "WARNING: Error checking SWIG version"
@@ -21,9 +21,12 @@ if 'commands' in globals():
         swig_version = output.split('\n')[1]
         if swig_version.find('SWIG Version 1.3.17') != 0:
             raise RuntimeError( "Wrong SWIG version: %s" % (swig_version,) )
-
+else:
+    print "WARNING: Error checking SWIG version"
+        
 swig_src_dir = os.path.split(sys.argv[0])[0]
-os.chdir(swig_src_dir)
+if swig_src_dir: # the above returns '' if we're in it on Python2.1
+    os.chdir(swig_src_dir)
 
 interfaces = ["darwin_app_stuff",
               "darwin_maxpriority",
@@ -36,12 +39,13 @@ end_names = ["_wrap.c",
 for i in interfaces:
     sys_string = "%s -python %s.i"%(swig_command,i)
     print sys_string
-    if 'commands' in globals():
+    if 'commands' in globals().keys():
         status, output = commands.getstatusoutput(sys_string)
         if status != 0:
             print "ERROR:", output
             raise RuntimeError("SWIG error")
     else:
+        print "WARNING: Cannot verify success of operation"
         os.system(sys_string)
     for end_name in end_names:
         filename = i + end_name
