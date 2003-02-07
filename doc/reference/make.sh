ac5fb2cd410376773bced4e3bf3a2115ce2112a0
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-07 02:17:07 +0000

    Minor: allow different versions of python when making docs
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@818 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- doc/reference/make.sh
+++ doc/reference/make.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-python pydoc.py -w VisionEgg
-python pydoc.py -w VisionEgg.Configuration
-python pydoc.py -w VisionEgg.Core
-python pydoc.py -w VisionEgg.Daq
-python pydoc.py -w VisionEgg.DaqLPT
-python pydoc.py -w VisionEgg.DaqOverTCP
-python pydoc.py -w VisionEgg.GUI
-python pydoc.py -w VisionEgg.Gratings
-python pydoc.py -w VisionEgg.Lib3DS
-python pydoc.py -w VisionEgg.MoreStimuli
-python pydoc.py -w VisionEgg.PlatformDependent
-python pydoc.py -w VisionEgg.PyroHelpers
-python pydoc.py -w VisionEgg.TCPController
-python pydoc.py -w VisionEgg.Text
-python pydoc.py -w VisionEgg.SphereMap
-python pydoc.py -w VisionEgg.Textures
+python=/usr/local/bin/python2.2
+$python pydoc.py -w VisionEgg
+$python pydoc.py -w VisionEgg.Configuration
+$python pydoc.py -w VisionEgg.Core
+$python pydoc.py -w VisionEgg.Daq
+$python pydoc.py -w VisionEgg.DaqLPT
+$python pydoc.py -w VisionEgg.DaqOverTCP
+$python pydoc.py -w VisionEgg.GUI
+$python pydoc.py -w VisionEgg.Gratings
+$python pydoc.py -w VisionEgg.Lib3DS
+$python pydoc.py -w VisionEgg.MoreStimuli
+$python pydoc.py -w VisionEgg.PlatformDependent
+$python pydoc.py -w VisionEgg.PyroHelpers
+$python pydoc.py -w VisionEgg.TCPController
+$python pydoc.py -w VisionEgg.Text
+$python pydoc.py -w VisionEgg.SphereMap
+$python pydoc.py -w VisionEgg.Textures
 
 
