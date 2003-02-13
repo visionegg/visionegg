be9a1d036ecce34c9c4b812f8b2e98fb8b824b07
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-13 21:46:11 +0000

    Bugfix: works again
    git-svn-id: https://visionegg.org/svn/trunk@835 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/lib3ds-demo.py
+++ visionegg/demo/lib3ds-demo.py
@@ -9,15 +9,17 @@ from VisionEgg.Lib3DS import *
 from VisionEgg.MoreStimuli import *
 from Numeric import *
 
-building_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/bldskyl1.3ds")
-fly_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/a3dwasp.3ds")
-truck_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/a3dsemi.3ds")
-car_file = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels/autshdm1.3DS")
+model_path = os.path.join(config.VISIONEGG_USER_DIR,"3dmodels")
+
+building_file = os.path.join(model_path,"bldskyl1.3ds")
+fly_file = os.path.join(model_path,"a3dwasp.3ds")
+truck_file = os.path.join(model_path,"a3dsemi.3ds")
+car_file = os.path.join(model_path,"autshdm1.3DS")
 
 # Because we can't include the .3ds files with the Vision Egg
 # (licensing issues), check to see if they are there and give warning
-# if not.  If someone wants to donate some nice .3ds file to the
-# Vision Egg project, that would be great!
+# if not.  If someone wants to donate some .3ds file to the Vision Egg
+# project, that would be great!
 
 error_string = (".3ds file \"%s\" not found.\nDownload from " +
     "http://www.amazing3d.com/free/free.html and try again.")
@@ -36,21 +38,23 @@ screen.parameters.bgcolor = (0.0,0.0,0.3,0.0)
 
 aspect = float(screen.size[0]) / (screen.size[1] / 2.0)
 
-buildings = Model3DS(filename=building_file)
+buildings = Model3DS(filename=building_file,
+                     shrink_texture_ok=1)
 truck = Model3DS(filename=truck_file,
                  position=array((-2000.0,0.0,-1000.0)),
                  orient_angle=90.0,
-                 orient_axis=array((0.0,1.0,0.0))                 
-                 )
+                 orient_axis=array((0.0,1.0,0.0)),
+                 shrink_texture_ok=1)
 car = Model3DS(filename=car_file,
-                 position=array((-2000.0,0.0,-1100.0)),
-                 orient_angle=90.0,
-                 orient_axis=array((0.0,1.0,0.0))                 
-                 )
+               position=array((-2000.0,0.0,-1100.0)),
+               orient_angle=90.0,
+               orient_axis=array((0.0,1.0,0.0)),
+               shrink_texture_ok=1)
 fly = Model3DS(filename=fly_file,
                scale=array((10.0,10.0,10.0)),
                orient_angle=90.0,
-               orient_axis=array((0.0,1.0,0.0)))
+               orient_axis=array((0.0,1.0,0.0)),
+               shrink_texture_ok=1)
 ground = Rectangle3D(color=(0.0,0.6,0.0,0.0),
                      vertex1=array(( -10000.0, 0.0, -10000.0)),
                      vertex2=array(( -10000.0, 0.0,  10000.0)),
@@ -102,7 +106,7 @@ def projection_matrix1_f(t):
     camera_look_at = fly_pos_f(t)
     camera_up = (0.0,1.0,0.0)
     projection.look_at( eye, camera_look_at, camera_up)
-    return projection.get_matrix()
+    return Numeric.array(projection.get_matrix())
 
 # This defines the perspective projection of the bottom viewport
 def projection_matrix2_f(t):
@@ -115,8 +119,7 @@ def projection_matrix2_f(t):
     camera_look_at = fly_pos_f(t) + array((-1.0,0.0,0.0)) # look ahead
     camera_up = (0.0,1.0,0.0)
     projection.look_at( eye, camera_look_at, camera_up)
-    return projection.get_matrix()
-
+    return Numeric.array(projection.get_matrix())
 
 p.add_controller(projection1,'matrix',
                  FunctionController(during_go_func=projection_matrix1_f))
