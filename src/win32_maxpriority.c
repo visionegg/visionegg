a8e31e58d0ae7cc2bbbc018ea7c67d5e6683a389
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-02-19 07:30:48 +0000

    Minor: functions return success values
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@863 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- src/win32_maxpriority.c
+++ src/win32_maxpriority.c
@@ -1,10 +1,10 @@
 #include <WINDOWS.H> // winbase.h is all we really want, but it doesn't include all the headers it needs
 #include <WINBASE.H>
 
-void set_self_process_priority_class( int priority_class ) {
-  return SetPriorityClass( GetCurrentProcess(), priority_class );
+unsigned short set_self_process_priority_class( int priority_class ) {
+  return (unsigned short)SetPriorityClass( GetCurrentProcess(), priority_class );
 }
 
-void set_self_thread_priority( int priority ) {
-  return SetThreadPriority( GetCurrentThread(), priority );
+unsigned short set_self_thread_priority( int priority ) {
+  return (unsigned short)SetThreadPriority( GetCurrentThread(), priority );
 }
