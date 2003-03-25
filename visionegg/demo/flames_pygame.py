e160ab04727289b7f68f31339e0006d3b64683bf
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-03-25 21:57:25 +0000

    Initial commit
    git-svn-id: https://visionegg.org/svn/trunk@961 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ visionegg/demo/flames_pygame.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+
+"""flames.py - Realtime Fire Effect Demo
+Pete Shinners, April 3, 2001
+
+Ok, this is a pretty intense demonstation of using
+the surfarray module and numeric. It uses an 8bit
+surfaces with a colormap to represent the fire. We
+then go crazy on a separate Numeric array to get
+realtime fire. I'll try to explain my methods here...
+
+This flame algorithm is very popular, and you can find
+it for just about all graphic libraries. The fire effect
+works by placing random values on the bottom row of the
+image. Then doing a simplish blur that is weighted to
+move the values upward. Then slightly darken the image
+so the colors get darker as they move up. The secret is
+that the blur routine is "recursive" so when you blur
+the 2nd row, the values there are used when you blur
+the 3rd row, and all the way up.
+
+This fire algorithm works great, but the bottom rows
+are usually a bit ugly. In this demo we just render
+a fire surface that has 3 extra rows at the bottom we
+just don't use.
+
+Also, the fire is rendered at half the resolution of
+the full image. We then simply double the size of the
+fire data before applying to the surface.
+
+Several of these techniques are covered in the pygame
+surfarray tutorial. doubling an image, and the fancy
+blur is just a modified version of what is in the tutorial.
+
+This runs at about 40fps on my celeron-400
+"""
+
+
+import pygame
+from pygame.surfarray import *
+from pygame.locals import *
+from Numeric import *
+from RandomArray import *
+
+RES = array((240, 180))
+MAX = 246
+RESIDUAL = 86
+HSPREAD, VSPREAD = 26, 78
+VARMIN, VARMAX = -2, 3
+
+def main():
+    "main function called when the script is run"
+    #first we just init pygame and create some empty arrays to work with    
+    pygame.init()
+    screen = pygame.display.set_mode(RES, 0, 8)
+    print screen.get_flags()
+    setpalette(screen)
+    flame = zeros(RES/2 + (0,3))
+    doubleflame = zeros(RES)
+    randomflamebase(flame)    
+
+    #the mainloop is pretty simple, the work is done in these funcs
+    while not pygame.event.peek((QUIT,KEYDOWN,MOUSEBUTTONDOWN)):
+        modifyflamebase(flame)
+        processflame(flame)
+        blitdouble(screen, flame, doubleflame)
+        pygame.display.flip()
+
+
+
+def setpalette(screen):
+    "here we create a numeric array for the colormap"
+    gstep, bstep = 75, 150
+    cmap = zeros((256, 3))
+    cmap[:,0] = minimum(arange(256)*3, 255)
+    cmap[gstep:,1] = cmap[:-gstep,0]
+    cmap[bstep:,2] = cmap[:-bstep,0]
+    screen.set_palette(cmap)
+
+
+def randomflamebase(flame):
+    "just set random values on the bottom row"
+    flame[:,-1] = randint(0, MAX, flame.shape[0])
+
+
+def modifyflamebase(flame):
+    "slightly change the bottom row with random values"
+    bottom = flame[:,-1]
+    mod = randint(VARMIN, VARMAX, bottom.shape[0])
+    add(bottom, mod, bottom)
+    maximum(bottom, 0, bottom)
+    #if values overflow, reset them to 0
+    bottom[:] = choose(greater(bottom,MAX), (bottom,0))
+
+
+def processflame(flame):
+    "this function does the real work, tough to follow"
+    notbottom = flame[:,:-1]    
+
+    #first we multiply by about 60%
+    multiply(notbottom, 146, notbottom)
+    right_shift(notbottom, 8, notbottom)
+
+    #work with flipped image so math accumulates.. magic!
+    flipped = flame[:,::-1]
+
+    #all integer based blur, pulls image up too
+    tmp = flipped * 20
+    right_shift(tmp, 8, tmp)
+    tmp2 = tmp >> 1
+    add(flipped[1:,:], tmp2[:-1,:], flipped[1:,:])
+    add(flipped[:-1,:], tmp2[1:,:], flipped[:-1,:])
+    add(flipped[1:,1:], tmp[:-1,:-1], flipped[1:,1:])
+    add(flipped[:-1,1:], tmp[1:,:-1], flipped[:-1,1:])
+
+    tmp = flipped * 80
+    right_shift(tmp, 8, tmp)
+    add(flipped[:,1:], tmp[:,:-1]>>1, flipped[:,1:])
+    add(flipped[:,2:], tmp[:,:-2], flipped[:,2:])
+
+    #make sure no values got too hot
+    minimum(notbottom, MAX, notbottom)
+
+
+def blitdouble(screen, flame, doubleflame):
+    "double the size of the data, and blit to screen"
+    doubleflame[::2,:-2:2] = flame[:,:-4]
+    doubleflame[1::2,:-2:2] = flame[:,:-4]
+    doubleflame[:,1:-2:2] = doubleflame[:,:-2:2]
+    blit_array(screen, doubleflame)
+
+
+
+if __name__ == '__main__': main()
+
+    
---
+++ visionegg/demo/flames_visionegg.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""flames_visionegg.py - a Vision Egg implementation of pygame flames.
+
+This demo renders a pygame surface (drawn with the flames demo of the
+PCR - pygame code repository) and then updates and OpenGL texture with
+surface using the Vision Egg."""
+
+import os
+import VisionEgg
+from VisionEgg.Core import *
+from VisionEgg.Text import *
+from VisionEgg.Textures import *
+import pygame.surface, pygame.locals
+import pygame.surfarray
+import OpenGL.GL as gl
+import flames_pygame as flames # "flames" from the pygame code repository
+
+screen = get_default_screen()
+
+# create 8 bit pygame surface
+pygame_surface = pygame.surface.Surface((256,128),0,8)
+# set the colormap of the pygame surface
+flames.setpalette(pygame_surface)
+# create a texture using this surface as the source of texel data
+texture = Texture(pygame_surface)
+
+# create an empty array
+flame = Numeric.zeros(texture.size)
+# fill it
+flames.randomflamebase(flame)
+
+text = Text( text = "Vision Egg/pygame flames demo - Press Esc to quit",
+             position = (screen.size[0]/2,2),
+             anchor = 'bottom',
+             color = (1.0,1.0,1.0,1.0))
+
+# Create the instance of TextureStimulus
+
+# (The bottom 3 pixels of the flames texture contains artifacts from
+# the flame-drawing algorithm, so put them below the visible portion
+# of the screen)
+
+hide_fraction = 3.0/texture.size[1]
+screen_hide_height = screen.size[1]*hide_fraction*2
+
+stimulus = TextureStimulus(texture = texture,
+                           position = (0,-screen_hide_height),
+                           anchor = 'lowerleft',
+                           size    = (screen.size[0],screen.size[1]+screen_hide_height), # scale to fit screen
+                           mipmaps_enabled = 0,
+                           texture_min_filter=gl.GL_LINEAR)
+
+texture_object = stimulus.parameters.texture.get_texture_object()
+def update_flames():
+    # this does the work of updating the flames
+    flames.modifyflamebase( flame )
+    flames.processflame( flame )
+    pygame.surfarray.blit_array(pygame_surface, flame)
+    texture_object.put_sub_image( pygame_surface )
+    
+viewport = Viewport(screen=screen,
+                    stimuli=[stimulus,text])
+
+p = Presentation(go_duration=('forever',),viewports=[viewport])
+
+def quit(dummy_arg=None):
+    p.parameters.go_duration = (0,'frames')
+    
+def keydown(event):
+    if event.key == pygame.locals.K_ESCAPE:
+        quit()
+        
+p.parameters.handle_event_callbacks=[(pygame.locals.QUIT, quit),
+                                     (pygame.locals.KEYDOWN, keydown)]
+p.add_controller(None,None,FunctionController(during_go_func=update_flames,
+                                              temporal_variables=Controller.TIME_INDEPENDENT))
+p.go()
