914f40b96852c6d3305a199f3500faefd74493ad
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-03-25 00:03:58 +0000

    Added framebuffer copy demo
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@957 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/multi_stim.py
+++ demo/multi_stim.py
@@ -9,9 +9,10 @@ from VisionEgg.Core import *
 from VisionEgg.Dots import DotArea2D
 from VisionEgg.Gratings import SinGrating2D
 from VisionEgg.MoreStimuli import Target2D
-from VisionEgg.Textures import Mask2D, Texture, SpinningDrum
+from VisionEgg.Textures import Mask2D, Texture, SpinningDrum, TextureStimulus
 from VisionEgg.Text import Text
 from math import sin, pi
+import math
 import pygame.locals
 
 #####################################
@@ -22,12 +23,13 @@ screen = get_default_screen()
 screen.parameters.bgcolor = (0.0,0.0,0.0,0.0) # make black (RGBA)
 
 x1 = screen.size[0]/4
-x2 = 3*screen.size[0]/4
+x2 = 2*screen.size[0]/4
+x3 = 3*screen.size[0]/4
 
-y1 = screen.size[1]/4
-y2 = 3*screen.size[1]/4
+y1 = screen.size[1]/3
+y2 = 2*screen.size[1]/3
 
-width = screen.size[0]/3
+width = screen.size[0]/5
 height = screen.size[1]/3
 
 #####################################
@@ -35,7 +37,7 @@ height = screen.size[1]/3
 #####################################
 
 text = Text( text = "Vision Egg multi stimulus demo - Press Esc to quit",
-             position = (screen.size[0]/2,0),
+             position = (screen.size[0]/2,2),
              anchor = 'bottom',
              color = (1.0,1.0,1.0,1.0))
 
@@ -67,7 +69,7 @@ color_grating = SinGrating2D(color1           = (0.5, 0.25, 0.5, 0.0), # RGBA, A
                              pedestal         = 0.1,
                              mask             = circle_mask,
                              center           = (x1,y2),
-                             size             = (width,width), # must be squre for circle shape
+                             size             = (width,width), # must be square for circle shape
                              spatial_freq     = 20.0/ screen.size[0],
                              temporal_freq_hz = 1.0,
                              orientation      = 270.0)
@@ -116,16 +118,41 @@ except Exception, x:
     gaussian_mask = None
 
 gray_rect = Target2D( center           = ( x2, y1 ),
-                      size             = ( width, width ),
+                      size             = ( width, height ),
                       color            = (0.5, 0.5, 0.5, 1.0),
                       )
 
 gabor = SinGrating2D(mask             = gaussian_mask,
                      center           = ( x2, y1 ),
-                     size             = ( width, width ),
+                     size             = ( width, height ),
                      spatial_freq     = 40.0 / screen.size[0], # units of cycles/pixel
                      temporal_freq_hz = 2.0,
                      orientation      = 45.0 )
+
+#########################
+#  Copy of framebuffer  #
+#########################
+
+# setup Texture instance for proper size and scaling, etc.
+framebuffer_copy_texture = Texture( texels=screen.get_framebuffer_as_image() )
+
+framebuffer_copy = TextureStimulus( texture = framebuffer_copy_texture,
+                                    mipmaps_enabled=0,
+                                    size=(width,height),
+                                    texture_min_filter=gl.GL_LINEAR,
+                                    position=(x3,y1),
+                                    anchor='center' )
+
+framebuffer_texture_object = framebuffer_copy.parameters.texture.get_texture_object()
+
+# OpenGL textures must be power of 2
+def next_power_of_2(f):
+    return math.pow(2.0,math.ceil(math.log(f)/math.log(2.0)))
+fb_width_pow2  = int(next_power_of_2(screen.size[0]))
+fb_height_pow2  = int(next_power_of_2(screen.size[1]))
+def copy_framebuffer():
+    "Replace contents of texture object with new texture copied from framebuffer"
+    framebuffer_texture_object.put_new_framebuffer(size=(fb_width_pow2,fb_height_pow2))
 
 ########################################
 #  Create viewports                    #
@@ -135,7 +162,8 @@ viewport_2d = Viewport( screen=screen, stimuli=[dots,
                                                 color_grating,
                                                 gray_rect,
                                                 gabor,
-                                                text] )
+                                                text,
+                                                framebuffer_copy] )
 
 drum_viewport = Viewport(screen=screen,
                          lowerleft=(x2-width/2,y2-height/2),
@@ -148,7 +176,7 @@ drum_viewport = Viewport(screen=screen,
 ########################################
 
 p = Presentation(go_duration=('forever',),
-                 viewports=[viewport_2d, drum_viewport])
+                 viewports=[drum_viewport, viewport_2d])
 
 def quit(dummy_arg=None):
     p.parameters.go_duration = (0,'frames')
@@ -167,5 +195,6 @@ p.parameters.handle_event_callbacks=[(pygame.locals.QUIT, quit),
 p.add_controller(color_grating,'pedestal',FunctionController(during_go_func=pedestal_func))
 p.add_controller(drum,'angular_position', FunctionController(during_go_func=angle_as_function_of_time))
 p.add_controller(drum_projection,'matrix', FunctionController(during_go_func=projection_matrix_f))
-
+p.add_controller(None,None, FunctionController(during_go_func=copy_framebuffer,
+                                               temporal_variables=Controller.TIME_INDEPENDENT))
 p.go()
