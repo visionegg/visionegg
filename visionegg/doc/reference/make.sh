c780a93209907477732acfa0516bbd878890748b
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-04-18 11:18:26 +0000

    Minor: make docs for most modules
    git-svn-id: https://visionegg.org/svn/trunk@996 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/doc/reference/make.sh
+++ visionegg/doc/reference/make.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
-python=/usr/local/bin/python2.2
+python=/usr/local/bin/python2.3
 $python pydoc.py -w VisionEgg
 $python pydoc.py -w VisionEgg.Configuration
 $python pydoc.py -w VisionEgg.Core
 $python pydoc.py -w VisionEgg.Daq
 $python pydoc.py -w VisionEgg.DaqLPT
 $python pydoc.py -w VisionEgg.DaqOverTCP
+$python pydoc.py -w VisionEgg.Dots
 $python pydoc.py -w VisionEgg.GUI
 $python pydoc.py -w VisionEgg.Gratings
 $python pydoc.py -w VisionEgg.Lib3DS
 $python pydoc.py -w VisionEgg.MoreStimuli
+$python pydoc.py -w VisionEgg.ParameterTypes
 $python pydoc.py -w VisionEgg.PlatformDependent
+$python pydoc.py -w VisionEgg.PyroClient
 $python pydoc.py -w VisionEgg.PyroHelpers
+$python pydoc.py -w VisionEgg.SphereMap
 $python pydoc.py -w VisionEgg.TCPController
 $python pydoc.py -w VisionEgg.Text
-$python pydoc.py -w VisionEgg.SphereMap
 $python pydoc.py -w VisionEgg.Textures
-
-
+$python pydoc.py -w VisionEgg.darwin_app_stuff
+$python pydoc.py -w VisionEgg.darwin_maxpriority
+$python pydoc.py -w VisionEgg.posix_maxpriority
+$python pydoc.py -w VisionEgg.win32_maxpriority
