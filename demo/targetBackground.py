ece3a4c86bf4b1479229127e1a678e6036498a6a
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-07-23 11:19:41 +0000

    Bugix: orientation of target Trivial: removed deprecated center parameter
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@1084 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- demo/targetBackground.py
+++ demo/targetBackground.py
@@ -22,7 +22,7 @@ screen = get_default_screen()
 # Create an instance of the Target2D class with appropriate parameters
 target = Target2D(size  = (25.0,10.0),
                   color      = (1.0,1.0,1.0,1.0), # Set the target color (RGBA) black
-                  orientation = 45.0)
+                  orientation = -45.0)
 
 # Create a viewport for the target
 target_viewport = Viewport(screen=screen, stimuli=[target])
@@ -81,7 +81,7 @@ drum_angle_controller = FunctionController(during_go_func=get_drum_angle)
 #  Connect the controllers with the variables they control  #
 #############################################################
 
-p.add_controller(target,'center', target_position_controller )
+p.add_controller(target,'position', target_position_controller )
 p.add_controller(drum,'angular_position', drum_angle_controller )
 
 #######################
