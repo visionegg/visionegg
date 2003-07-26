b69deed6b062930375c5635a59d9b5ec0013f9bf
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-07-26 14:46:45 +0000

    Initial commit
    git-svn-id: https://visionegg.org/svn/trunk@1088 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ visionegg/demo/daq/simple_lpt_out.py
@@ -0,0 +1,14 @@
+from VisionEgg.DaqLPT import raw_lpt_module
+import time
+
+LPT1 = 0x378 # address of parallel port -- make sure your computer agrees
+
+# turn all pins on
+data = 0xFF # values to output on parallel port
+raw_lpt_module.out(LPT1,data)
+
+time.sleep(1.0) # wait 1 second
+
+# turn all pins off
+data = 0x00
+raw_lpt_module.out(LPT1,data)
