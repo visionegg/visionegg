6ac5fa03b976a69d39146fba4d69edf6e3a17b7c
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-07-26 15:10:41 +0000

    Major: more arguments supported, bitmasks supported, attch() function added
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@1101 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- src/GLTrace.py
+++ src/GLTrace.py
@@ -13,8 +13,6 @@ in your code.
 
 Also, trace another module's use of OpenGL by changing its reference
 to OpenGL.GL to a reference to VisionEgg.GLTrace.
-
-This module also imports everything in OpenGL.GL.ARB.multitexture.
 """
 
 # Copyright (c) 2003 Andrew Straw.  Distributed under the terms of the
@@ -28,13 +26,6 @@ This module also imports everything in OpenGL.GL.ARB.multitexture.
 
 import OpenGL.GL as gl
 
-import OpenGL.GL.ARB.multitexture
-for attr in dir(OpenGL.GL.ARB.multitexture):
-    # put attributes from multitexture module in "gl" module dictionary
-    # (Namespace overlap as you'd get OpenGL apps written in C)
-    if attr[0:2] != "__":
-        setattr(gl,attr,getattr(OpenGL.GL.ARB.multitexture,attr))
-
 import string        
 __cvs__ = string.split('$Revision$')[1]
 __date__ = string.join(string.split('$Date$')[1:3], ' ')
@@ -42,53 +33,98 @@ __author__ = 'Andrew Straw <astraw@users.sourceforge.net>'
 
 gl_constants = {}
 
-# functions which we don't want to map all the arguments of
-special_functions = {
-    'glOrtho':[0,1,2,3,4,5], # x,y,width,height
-    'glReadPixels':[0,1,2,3], # x,y,width,height
-    'glTexImage2D':[1,3,4,5], # level,width,height,border
-    'glVertex2f':[0,1], # all arguments
+# functions which we don't want to translate arguments to gl constant
+raw_args_by_function = {
+    'glColor':[0,1,2,3],
+    'glColorf':[0,1,2,3],
+    'glDepthRange':[0,1],
+    'glGenTextures':[0],
+    'glGetTexLevelParameteriv':[1],
+    'glOrtho':[0,1,2,3,4,5],
+    'glReadPixels':[0,1,2,3],
+    'glRotate':[0,1,2,3],
+    'glTexImage1D':[1,3,4],
+    'glTexImage2D':[1,3,4,5],
+    'glTexSubImage1D':[1,2,3],
+    'glTranslate':[0,1,2],
+    'glVertex2f':[0,1],
+    'glViewport':[0,1,2,3],
+    }
+
+bitmasks_by_function = {
+    'glClear':[0],
+    }
+
+bitmask_names_by_value = {
+    gl.GL_COLOR_BUFFER_BIT : 'GL_COLOR_BUFFER_BIT',
+    gl.GL_DEPTH_BUFFER_BIT : 'GL_DEPTH_BUFFER_BIT',
     }
 
 def arg_to_str( arg ):
     if type(arg) == int:
         if arg in gl_constants.keys():
             return gl_constants[arg]
-    elif type(arg) == str:
-        return "'<string>'"
+    elif type(arg) == str and len(arg) > 30:
+            return "'<string>'"
     return repr(arg)
 
 class Wrapper:
-    def __init__(self, attr_name):
-        self.attr_name = attr_name
-        self.orig_func = getattr(gl,self.attr_name)
+    def __init__(self, function_name):
+        self.function_name = function_name
+        self.orig_func = getattr(gl,self.function_name)
     def run(self,*args,**kw):
         if kw: kw_str = " AND KEYWORDS"
         else: kw_str = ""
-        if self.attr_name in special_functions:
+        if 1:
+##        if self.function_name in raw_args_by_function:
             arg_str = []
             for i in range(len(args)):
-                if i in special_functions[self.attr_name]:
+                if self.function_name in raw_args_by_function and i in raw_args_by_function[self.function_name]:
                     arg_str.append(str(args[i])) # don't convert to name of OpenGL constant
+                elif self.function_name in bitmasks_by_function and i in bitmasks_by_function[self.function_name]:
+                    bitmask_strs = []
+                    value = args[i]
+                    for bit_value in bitmask_names_by_value:
+                        if value & bit_value:
+                            value = value & ~bit_value
+                            bitmask_strs.append( bitmask_names_by_value[bit_value] )
+                    if value != 0:
+                        bitmask_strs.append( arg_to_str(args[i]) )
+                    arg_str.append( '|'.join(bitmask_strs) )
                 else:
                     arg_str.append(arg_to_str(args[i])) # convert to name of OpenGL constant
             arg_str = string.join( arg_str, "," )
+##        else:
+##            arg_str = string.join( map( arg_to_str, args ), "," )
+        try:
+            result = self.orig_func(*args,**kw)
+        except:
+            print "%s(%s)%s"%(self.function_name,arg_str,kw_str)
+            raise
+
+        if result is not None:
+            result_str = "%s = "%(arg_to_str(result),)
         else:
-            arg_str = string.join( map( arg_to_str, args ), "," )
-        print "%s(%s)%s"%(self.attr_name,arg_str,kw_str)
-        result = self.orig_func(*args,**kw)
+            result_str = ''
+        print "%s%s(%s)%s"%(result_str,self.function_name,arg_str,kw_str)
         return result
 
-for attr in dir(gl):
-    if callable( getattr(gl,attr) ):
-        wrapper = Wrapper(attr)
-        cmd = "%s = wrapper.run"%attr
-        exec cmd
-    else:
-        cmd = "%s = getattr(gl,'%s')"%(attr,attr)
-        exec cmd
-        if attr[:2] != '__' and attr[-2:] != '__':
-            if type(getattr(gl,attr)) == int:
-                if getattr(gl,attr) > 256: # assume first n aren't constants (n is arbitrary choice)
-                    gl_constants[getattr(gl,attr)] = attr
-        
+def attach():
+    for attr_name in dir(gl):
+        if callable( getattr(gl,attr_name) ):
+            wrapper = Wrapper(attr_name)
+            #cmd = "%s = wrapper.run"%attr_name
+            #exec cmd
+            globals()[attr_name] = wrapper.run
+        else:
+            #cmd = "%s = getattr(gl,'%s')"%(attr_name,attr_name)
+            #print cmd
+            attr = getattr(gl,attr_name)
+            globals()[attr_name] = attr
+            if not attr_name.startswith('__') and not attr_name.startswith('__'):
+                if type(getattr(gl,attr_name)) == int:
+                    gl_constants[getattr(gl,attr_name)] = attr_name
+##                    if getattr(gl,attr) > 256: # assume first n aren't constants (n is arbitrary choice)
+##                        gl_constants[getattr(gl,attr)] = attr
+
+attach() # attach when imported
