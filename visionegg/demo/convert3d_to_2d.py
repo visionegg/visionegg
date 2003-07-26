63ab5d774db4f090941dda3dcdc5b799f3ec0952
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-07-26 14:46:00 +0000

    Major: move stimulus during presentation
    git-svn-id: https://visionegg.org/svn/trunk@1087 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/convert3d_to_2d.py
+++ visionegg/demo/convert3d_to_2d.py
@@ -1,69 +1,82 @@
 #!/usr/bin/env python
 """Convert 3D position to 2D position"""
 
+import VisionEgg
 from VisionEgg.Core import *
 from VisionEgg.Text import *
-from VisionEgg.MoreStimuli import *
+from VisionEgg.Textures import *
 
 screen = get_default_screen()
-screen.parameters.bgcolor = 0.0, 0.0, 0.6 # background blue
+screen.parameters.bgcolor = 0.0, 0.0, 0.0 # background black
 
-l = 0
+l = -0.1
 r = 0.5
 t = 0.3
 b = 0
-z = -1
+z = 0
 
 vertices = [(l,b,z),
-     (l,t,z),
-     (r,t,z),
-     (r,b,z)]
+            (r,b,z),
+            (l,t,z),
+            (r,t,z)]
 
-rect = Rectangle3D(color=(1.0,0.3,1.0),
-                   vertex1=vertices[0],
-                   vertex2=vertices[1],
-                   vertex3=vertices[2],
-                   vertex4=vertices[3],
-                   )
+# Get a texture
+filename = os.path.join(VisionEgg.config.VISIONEGG_SYSTEM_DIR,"data","visionegg.png")
+texture = Texture(filename)
+
+rect = TextureStimulus3D(texture=texture,
+                         lowerleft=vertices[0],
+                         lowerright=vertices[1],
+                         upperleft=vertices[2],
+                         upperright=vertices[3],
+                         )
 
 viewport2D = Viewport(screen=screen)
 projection2D = viewport2D.parameters.projection
 
-# specify whatever projection you want here
+# This gets updated in the function below
 projection3D = SimplePerspectiveProjection(fov_x=90.0)
-# arbitrary manipulations just to prove point
-projection3D.rotate(20.0,0.0,1.0,1.0)
-projection3D.translate(-.2,.1,0.02)
+
+def update(t):
+    projection = SimplePerspectiveProjection(fov_x=90.0)
+    projection.translate(0,0,-1)
+    projection.rotate(36.0*t,0.0,1.0,0)
+    projection3D.parameters.matrix = projection.get_matrix()
+    for i in range(len(vertex_labels)):
+        vertex = vertices[i]
+        text = vertex_labels[i]
+        window_coords = viewport3D.eye_2_window(vertex)
+        text.parameters.text='<- %.1f, %.1f'%(window_coords[0],window_coords[1])
+        text.parameters.position = window_coords[0],window_coords[1]
 
 viewport3D = Viewport(screen=screen,
               projection=projection3D,
               stimuli=[rect])
     
-ortho_stim = []
+vertex_labels = []
 for vertex in vertices:
-    window_coords = viewport3D.eye_2_window(vertex)
-    ortho_stim.append(
-        Text(text='<- %.1f, %.1f'%(window_coords[0],window_coords[1]),
-             position=window_coords[:2],
+    vertex_labels.append(
+        Text(text='temporary text',
              anchor='left',
              )
         )
-    
-ortho_stim.append(
+
+other_text = []
+other_text.append(
     Text(text='Pixel positions (x,y) calculated from 3D coordinates',
          position=(screen.size[0]/2,screen.size[1]),
          anchor='top',
          )
     )
 
-ortho_stim.append(
+other_text.append(
     Text(text='----> x',
          position=(10,10),
          anchor='left',
          )
     )
 
-ortho_stim.append(
+other_text.append(
     Text(text='----> y',
          angle=90.0,
          position=(10,10),
@@ -71,7 +84,8 @@ ortho_stim.append(
          )
     )
 
-viewport2D.parameters.stimuli = ortho_stim
+viewport2D.parameters.stimuli = vertex_labels + other_text
 
 p = Presentation(go_duration=(10.0,'seconds'),viewports=[viewport3D,viewport2D])
+p.add_controller(None, None, FunctionController(during_go_func=update))
 p.go()
