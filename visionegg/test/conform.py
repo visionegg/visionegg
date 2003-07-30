8d7ed77755d8b999408840481712049aaf1b193d
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2003-07-30 09:33:35 +0000

    Minor: added tests for most Stimulus classes.
    git-svn-id: https://visionegg.org/svn/trunk@1123 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/test/conform.py
+++ visionegg/test/conform.py
@@ -7,6 +7,16 @@ import VisionEgg
 import VisionEgg.Core
 import OpenGL.GL as gl
 
+# for some specific test cases:
+import VisionEgg.Dots
+import VisionEgg.Gratings
+import VisionEgg.SphereMap
+import VisionEgg.Textures
+import Numeric
+import Image
+import ImageDraw
+import os
+
 if DEBUG:
     import VisionEgg.GLTrace
     VisionEgg.Core.gl = VisionEgg.GLTrace
@@ -26,15 +36,22 @@ class VETestCase(unittest.TestCase):
                                              maxpriority   = False,
                                              hide_mouse    = False,
                                              frameless     = False,
-                                             bgcolor       = (0,0,1), # Blue (RGB)
+                                             bgcolor       = (0,0,0), # Black (RGB)
                                              )
         self.screen.clear()
         VisionEgg.Core.swap_buffers()
         self.ortho_viewport = VisionEgg.Core.Viewport( screen = self.screen )
         
     def tearDown(self):
-        VisionEgg.Core.swap_buffers()
+        VisionEgg.Core.swap_buffers() # just for a brief flash...
         del self.screen
+
+    def test_core_presentation_go(self):
+        p = VisionEgg.Core.Presentation(go_duration=(5,'frames'))
+        p.go()
+        p.go() # check to make sure it works a second time
+        p.parameters.go_duration = (0,'frames')
+        p.go() # make sure it works with 0 duration
 
     def test_core_screen_query_refresh_rate(self):
         fps = self.screen.query_refresh_rate()
@@ -53,15 +70,49 @@ class VETestCase(unittest.TestCase):
         percent_diff = abs(fps1-fps2)/max(fps1,fps2)*100.0
         self.failUnless(percent_diff < 5.0,'measured and queried frame rates different (swap buffers may not be synced to vsync)')
 
+    def test_dots_dotarea2d(self):
+        stimulus = VisionEgg.Dots.DotArea2D()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_gratings_singrating2d(self):
+        stimulus = VisionEgg.Gratings.SinGrating2D()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_gratings_singrating2d_2colors(self):
+        stimulus = VisionEgg.Gratings.SinGrating2D(color1=(1,0,0),
+                                                  color2=(0,0,1))
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_gratings_singrating2d_mask(self):
+        mask = VisionEgg.Textures.Mask2D()
+        stimulus = VisionEgg.Gratings.SinGrating2D(mask=mask)
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_spheremap_azelgrid(self):
+        stimulus = VisionEgg.SphereMap.AzElGrid(my_viewport=self.ortho_viewport)
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_spheremap_spheremap(self):
+        filename = os.path.join(VisionEgg.config.VISIONEGG_SYSTEM_DIR,"data","az_el.png")
+        texture = VisionEgg.Textures.Texture(filename)
+        stimulus = VisionEgg.SphereMap.SphereMap(texture=texture,
+                                                 shrink_texture_ok=True)
+        self.ortho_viewport.parameters.stimuli = [ stimulus ] 
+        self.ortho_viewport.draw()
+
+    def test_spheremap_spherewindow(self):
+        stimulus = VisionEgg.SphereMap.SphereWindow()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ] 
+        self.ortho_viewport.draw()
+
     def test_texture_pil(self):
         if DEBUG:
             print "test_texture_pil",'*'*80
-        
-        import VisionEgg.Textures
-        if DEBUG:
-            VisionEgg.Textures.gl = VisionEgg.GLTrace
-        import Image
-        import ImageDraw
         
         width, height = self.screen.size
         orig = Image.new("RGB",(width,height),(0,0,0))
@@ -72,15 +123,24 @@ class VETestCase(unittest.TestCase):
         result = texture.get_texels_as_image()
         self.failUnless(result.tostring()==orig.tostring(),'exact texture reproduction with PIL textures failed')
 
+    def test_texture_stimulus_3d(self):
+        stimulus = VisionEgg.Textures.TextureStimulus3D()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_textures_spinning_drum(self):
+        stimulus = VisionEgg.Textures.SpinningDrum()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
+    def test_textures_fixation_cross(self):
+        stimulus = VisionEgg.Textures.FixationCross()
+        self.ortho_viewport.parameters.stimuli = [ stimulus ]
+        self.ortho_viewport.draw()
+
     def test_texture_stimulus_pil(self):
         if DEBUG:
             print "test_texture_stimulus_pil",'*'*80
-
-        import VisionEgg.Textures
-        if DEBUG:
-            VisionEgg.Textures.gl = VisionEgg.GLTrace
-        import Image
-        import ImageDraw
 
         width, height = self.screen.size
         orig = Image.new("RGB",(width,height),(0,0,0))
@@ -96,7 +156,6 @@ class VETestCase(unittest.TestCase):
             )
 
         self.ortho_viewport.parameters.stimuli = [ texture_stimulus ]
-        # XXX should add self.screen.clear() ??
         self.ortho_viewport.draw()
         result = self.screen.get_framebuffer_as_image()
         self.failUnless(result.tostring()==orig.tostring(),'exact texture stimulus reproduction with PIL textures failed')
@@ -105,11 +164,6 @@ class VETestCase(unittest.TestCase):
         if DEBUG:
             print "test_texture_stimulus_numpy",'*'*80
         
-        import VisionEgg.Textures
-        if DEBUG:
-            VisionEgg.Textures.gl = VisionEgg.GLTrace
-        import Numeric
-
         width, height = self.screen.size
 
         orig = Numeric.zeros((height,width,3),Numeric.UnsignedInt8)
@@ -126,7 +180,6 @@ class VETestCase(unittest.TestCase):
             )
 
         self.ortho_viewport.parameters.stimuli = [ texture_stimulus ]
-        # XXX should add self.screen.clear() ??
         self.ortho_viewport.draw()
         result = self.screen.get_framebuffer_as_array()
         if DEBUG:
@@ -143,13 +196,25 @@ class VETestCase(unittest.TestCase):
             print orig[-10:,-10:,0]
             print result[-10:,-10:,0]
         self.failUnless(Numeric.allclose(orig,result),'exact texture reproduction with Numeric textures failed')
+        
 
 def suite():
     ve_test_suite = unittest.TestSuite()
+    ve_test_suite.addTest( VETestCase("test_core_presentation_go") )
+    ve_test_suite.addTest( VETestCase("test_core_refresh_rates_match") )
     ve_test_suite.addTest( VETestCase("test_core_screen_query_refresh_rate") )
     ve_test_suite.addTest( VETestCase("test_core_screen_measure_refresh_rate") )
-    ve_test_suite.addTest( VETestCase("test_core_refresh_rates_match") )
+    ve_test_suite.addTest( VETestCase("test_dots_dotarea2d") )
+    ve_test_suite.addTest( VETestCase("test_gratings_singrating2d") )
+    ve_test_suite.addTest( VETestCase("test_gratings_singrating2d_mask") )
+    ve_test_suite.addTest( VETestCase("test_gratings_singrating2d_2colors") )
+    ve_test_suite.addTest( VETestCase("test_spheremap_azelgrid") )
+    ve_test_suite.addTest( VETestCase("test_spheremap_spheremap") )
+    ve_test_suite.addTest( VETestCase("test_spheremap_spherewindow") )
     ve_test_suite.addTest( VETestCase("test_texture_pil") )
+    ve_test_suite.addTest( VETestCase("test_texture_stimulus_3d") )
+    ve_test_suite.addTest( VETestCase("test_textures_spinning_drum") )
+    ve_test_suite.addTest( VETestCase("test_textures_fixation_cross") )
     ve_test_suite.addTest( VETestCase("test_texture_stimulus_pil") )
     ve_test_suite.addTest( VETestCase("test_texture_stimulus_numpy") )
     return ve_test_suite
