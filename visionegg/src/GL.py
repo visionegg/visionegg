537d1ae7999bac3fc881ad691e139128d70e03fb
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2005-01-19 08:29:57 +0000

    Explicit import statements for OpenGL modules so that py2exe picks them up.
    git-svn-id: https://visionegg.org/svn/trunk@1312 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/src/GL.py
+++ visionegg/src/GL.py
@@ -16,8 +16,26 @@ Vision Egg GL module -- lump all OpenGL names in one namespace.
 
 from OpenGL.GL import * # get everything from OpenGL.GL
 
+# tell py2exe we want these modules
+try:
+    import OpenGL.GL.GL__init___
+except:
+    pass
+try:
+    import OpenGL.GL.ARB.multitexture
+except:
+    pass
+try:
+    import OpenGL.GL.EXT.bgra
+except:
+    pass
+try:
+    import SGIS.texture_edge_clamp
+except:
+    pass
+
+# why doesn't PyOpenGL define this?!
 try:
     GL_UNSIGNED_INT_8_8_8_8_REV
 except NameError:
-    GL_UNSIGNED_INT_8_8_8_8_REV = 0x8367 # XXX why doesn't PyOpenGL define this?!
-
+    GL_UNSIGNED_INT_8_8_8_8_REV = 0x8367 
