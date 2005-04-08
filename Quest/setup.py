27bff83272dbd4d4fdeb845fec133bd18f762697
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2005-04-08 18:00:42 +0000

    Initial revision
    git-svn-id: https://visionegg.org/svn/trunk@1315 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ Quest/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Quest',
+    version='1.0',
+    description = "QUEST algorithm for finding threshold",
+    author = "Dennis Pelli",
+    maintainer = "Andrew D Straw",
+    license = "BSD",
+    packages = [''],
+    package_dir = {'':'src'},
+    )
