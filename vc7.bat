5db52fed18d25d81fb19db1f4204f1d74b3a35c0
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2006-04-03 00:08:43 +0000

    got compilation working with latest microsoft free tools
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@1368 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ vc7.bat
@@ -0,0 +1,11 @@
+rem Modified from http://www.vrplumber.com/programming/mstoolkit/
+
+rem vc7.bat, copied to the command path for the machine
+@echo off
+
+Set PATH=C:\Program Files\Microsoft Visual C++ Toolkit 2003\bin;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Bin;%PATH%
+Set INCLUDE=C:\Program Files\Microsoft Visual C++ Toolkit 2003\include;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\include;%INCLUDE%
+Set LIB=C:\Program Files\Microsoft Visual Studio 8\VC\lib;C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib;C:\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib;%LIB%
+
+echo Visit http://msdn.microsoft.com/visualc/using/documentation/default.aspx for
+echo complete compiler documentation.
