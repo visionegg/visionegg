03fc0b9cbd2fac9623108be0f7eb73aa6f383422
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2007-09-17 16:10:36 +0000

    add new example of using FRAMES_ABSOLUTE
    git-svn-id: https://visionegg.org/svn/trunk/visionegg@1401 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

---
+++ demo/framecount.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""Use frame information to display stimuli."""
+
+import VisionEgg
+VisionEgg.start_default_logging(); VisionEgg.watch_exceptions()
+
+from VisionEgg.Core import get_default_screen, Viewport
+from VisionEgg.FlowControl import Presentation, FunctionController, FRAMES_ABSOLUTE
+from VisionEgg.Text import Text
+
+screen = get_default_screen()
+screen.parameters.bgcolor = (0.0,0.0,1.0) # background blue (RGB)
+
+text = Text(text="Hello world!",
+            color=(1.0,1.0,1.0), # alpha is ignored (set with max_alpha_param)
+            position=(screen.size[0]/2,screen.size[1]/2),
+            font_size=50,
+            anchor='center')
+
+viewport = Viewport(screen=screen,
+                    size=screen.size,
+                    stimuli=[text])
+
+def text_func(f_abs):
+    return "framecount: %d"%f_abs
+
+t_controller = FunctionController(during_go_func=text_func,
+                                  temporal_variables = FRAMES_ABSOLUTE)
+
+p = Presentation(go_duration=(5.0,'seconds'),viewports=[viewport])
+p.add_controller(text,'text',t_controller)
+p.go()
