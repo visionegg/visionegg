ce8e6851c763dc1b3f5ef937359a306fde6b5796
astraw <astraw@3a63a0ee-37fe-0310-a504-e92b6e0a3ba7>
2007-09-17 16:14:20 +0000

    text doesn't shift laterally as width changes
    git-svn-id: https://visionegg.org/svn/trunk@1402 3a63a0ee-37fe-0310-a504-e92b6e0a3ba7

--- visionegg/demo/framecount.py
+++ visionegg/demo/framecount.py
@@ -11,22 +11,21 @@ from VisionEgg.Text import Text
 screen = get_default_screen()
 screen.parameters.bgcolor = (0.0,0.0,1.0) # background blue (RGB)
 
-text = Text(text="Hello world!",
-            color=(1.0,1.0,1.0), # alpha is ignored (set with max_alpha_param)
-            position=(screen.size[0]/2,screen.size[1]/2),
-            font_size=50,
-            anchor='center')
-
-viewport = Viewport(screen=screen,
-                    size=screen.size,
-                    stimuli=[text])
+textvar = Text(color=(1.0,1.0,1.0), # alpha is ignored (set with max_alpha_param)
+               position=(screen.size[0]/4,screen.size[1]/2),
+               font_size=50,
+               anchor='left')
 
 def text_func(f_abs):
-    return "framecount: %d"%f_abs
+    return "framecount: % 4d"%f_abs
 
 t_controller = FunctionController(during_go_func=text_func,
                                   temporal_variables = FRAMES_ABSOLUTE)
 
+viewport = Viewport(screen=screen,
+                    size=screen.size,
+                    stimuli=[textvar])
+
 p = Presentation(go_duration=(5.0,'seconds'),viewports=[viewport])
-p.add_controller(text,'text',t_controller)
+p.add_controller(textvar,'text',t_controller)
 p.go()
